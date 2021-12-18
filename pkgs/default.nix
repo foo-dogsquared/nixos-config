e956886da826ab7ff4dd0feebf8d2f672300c70d
Gabriel Arazas <foo.dogsquared@gmail.com>
2021-12-18 17:36:19 +0800

    Format `pkgs/default.nix`

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,17 +1,17 @@
 { pkgs ? import <nixpkgs> { } }:
 
-{
-  doggo = pkgs.callPackage ./doggo.nix { };
+with pkgs; {
+  doggo = callPackage ./doggo.nix { };
   gnome-shell-extension-pop-shell =
-    pkgs.callPackage ./gnome-shell-extension-pop-shell.nix { };
-  libcs50 = pkgs.callPackage ./libcs50.nix { };
-  pop-launcher = pkgs.callPackage ./pop-launcher.nix { };
+    callPackage ./gnome-shell-extension-pop-shell.nix { };
   gnome-shell-extension-fly-pie =
     callPackage ./gnome-shell-extension-fly-pie.nix { };
+  libcs50 = callPackage ./libcs50.nix { };
   llama = callPackage ./llama.nix { };
   neo = callPackage ./neo.nix { };
+  pop-launcher = callPackage ./pop-launcher.nix { };
   pop-launcher-plugin-duckduckgo-bangs =
-    pkgs.callPackage ./pop-launcher-plugin-duckduckgo-bangs.nix { };
-  tic-80 = pkgs.callPackage ./tic-80.nix { };
+    callPackage ./pop-launcher-plugin-duckduckgo-bangs.nix { };
+  tic-80 = callPackage ./tic-80 { };
   sioyek = libsForQt5.callPackage ./sioyek.nix { };
 }
