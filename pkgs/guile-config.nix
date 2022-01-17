22d734bdc64b65e62219684ec342463b030f8cd6
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-01-17 15:28:55 +0800

    guile-config: init at 0.5.0

---
+++ pkgs/guile-config.nix
@@ -0,0 +1,28 @@
+{ stdenv, lib, guile_3_0, fetchFromGitLab, autoreconfHook, pkg-config, texinfo
+}:
+
+stdenv.mkDerivation rec {
+  pname = "guile-config";
+  version = "0.5.0";
+
+  src = fetchFromGitLab {
+    owner = "a-sassmannshausen";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-8Ma2pzqR8il+8H6WVbYLpKBk2rh3aKBr1mvvzdpCNPc=";
+  };
+
+  nativeBuildInputs = [ pkg-config autoreconfHook texinfo ];
+  propagatedBuildInputs = [ guile_3_0 ];
+
+  preConfigure = ''
+    sed -i '/guilemoduledir\s*=/s%=.*%=''${out}/share/guile/site%' configure.ac
+    sed -i '/guileobjectdir\s*=/s%=.*%=''${out}/share/guile/ccache%' configure.ac
+  '';
+
+  meta = with lib; {
+    description = "Library for a declarative approach for configuration";
+    homepage = "https://gitlab.com/a-sassmannshausen/guile-config";
+    license = licenses.gpl3;
+  };
+}
