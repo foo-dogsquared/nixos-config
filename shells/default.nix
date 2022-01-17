06db2633f5dfbd4ff2e8a0ddceb31fd6938d6cd0
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-01-17 14:51:41 +0800

    Add `gnu` devshell for GNU projects

--- shells/default.nix
+++ shells/default.nix
@@ -2,6 +2,7 @@
 
 with pkgs; {
   flatpak = callPackage ./flatpak.nix { };
+  gnu = callPackage ./gnu.nix { };
   hugo = callPackage ./hugo.nix { };
   rust = callPackage ./rust.nix { };
   tic-80 = callPackage ./tic-80.nix { };
---
+++ shells/gnu.nix
@@ -0,0 +1,17 @@
+# The GNU build system. Commonly used for projects at GNU and anyone who wants
+# to be a GNU fanatic.
+#
+# It's a good thing they have documented the full details in one of their
+# manuals at
+# https://www.gnu.org/software/automake/manual/html_node/GNU-Build-System.html
+{ mkShell, lib, autoconf, autoconf-archive, automake, gnumake, gcc }:
+
+mkShell {
+  packages = [
+    autoconf
+    autoconf-archive
+    automake
+    gnumake
+    gcc
+  ];
+}
