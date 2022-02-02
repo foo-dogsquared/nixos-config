c9e4e52262b87b7a8f545f267e5f949763c0cfcc
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-02-02 12:25:52 +0800

    junction: 1.4.0 -> 1.5.0

--- pkgs/junction.nix
+++ pkgs/junction.nix
@@ -1,28 +1,33 @@
 { lib, stdenv, fetchFromGitHub, desktop-file-utils, gjs, appstream-glib
-, gobject-introspection, gsettings-desktop-schemas, libadwaita, meson_0_60
+, gobject-introspection, gsettings-desktop-schemas, libadwaita, meson
 , ninja, wrapGAppsHook4 }:
 
 stdenv.mkDerivation rec {
   pname = "junction";
-  version = "1.4.0";
+  version = "1.5.0";
 
   src = fetchFromGitHub {
     owner = "sonnyp";
     repo = "Junction";
     rev = "v${version}";
-    sha256 = "sha256-lU6dGsX6jYYmwRz0z4V+4xTFAikUYCsszNVoyc7snwM=";
+    sha256 = "sha256-jS4SHh1BB8jk/4EP070X44C4n3GjyCz8ozgK8v5lbqc=";
   };
 
   nativeBuildInputs = [
-    meson_0_60
+    meson
     ninja
     wrapGAppsHook4
-    gobject-introspection
-    appstream-glib
     desktop-file-utils
   ];
 
-  buildInputs = [ libadwaita gjs gsettings-desktop-schemas ];
+  buildInputs = [ gsettings-desktop-schemas ];
+
+  propagatedBuildInputs = [
+    appstream-glib
+    gjs
+    gobject-introspection
+    libadwaita
+  ];
 
   preFixup = ''
     substituteInPlace $out/bin/re.sonny.Junction --replace "#!/usr/bin/env -S gjs" "#!${gjs}/bin/gjs"
