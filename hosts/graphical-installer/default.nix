d225257f8d8b4129ddc3b07431ef5e06a37cdcd3
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-03-01 23:07:56 +0800

    hosts: init `graphical-installer`
    This is my personalized graphical installer to make installing NixOS a
    little easier with all of my comfy config. Though, this won't be seeing
    a release as the resulting file is over GitHub's asset size limit of
    2GB. Man...

---
+++ hosts/graphical-installer/default.nix
@@ -0,0 +1,49 @@
+{ self, lib, config, pkgs, inputs, modulesPath, ... }:
+
+{
+  imports = [
+    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-base.nix"
+    (lib.getUser "nixos" "nixos")
+  ];
+
+  networking.hostName = "graphical-installer";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
+  users.users.root.password = "";
+
+  isoImage = {
+    isoBaseName = config.networking.hostName;
+    contents = [{
+      source = self;
+      target = "/bootstrap/";
+    }];
+    storeContents = [
+      self.devShell.${config.nixpkgs.system}
+    ] ++ builtins.attrValues inputs;
+  };
+
+  profiles = {
+    desktop = {
+      enable = true;
+      fonts.enable = true;
+    };
+    dev = {
+      enable = true;
+      shell.enable = true;
+      neovim.enable = true;
+    };
+  };
+
+  themes.themes.a-happy-gnome.enable = true;
+  services.xserver.displayManager = {
+    gdm = {
+      enable = true;
+      autoSuspend = false;
+    };
+    autoLogin = {
+      enable = true;
+      user = "nixos";
+    };
+  };
+}
