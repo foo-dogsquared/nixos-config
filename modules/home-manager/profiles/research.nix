fe94062d2edb8d250e1bc72d9e4550ff22591ee9
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-04-03 10:24:10 +0800

    profiles/research: add more applications for it

--- modules/home-manager/profiles/research.nix
+++ modules/home-manager/profiles/research.nix
@@ -10,9 +10,12 @@ in {
       archivebox # The ultimate archiving solution created by a pirate!
       curl # The general purpose downloader.
       fanficfare # It's for the badly written fanfics.
+      gallery-dl # More potential for your image collection.
       internetarchive # All of the potential vintage collection of questionable materials at your fingertips.
+      monolith # Archive webpages into a single file.
       newsboat # Reading news easily on the command line?
       qbittorrent # The pirate's toolkit for downloading Linux ISOs.
+      wget # Who would've think a simple tool can be made for this purpose?
       yt-dlp # The general purpose video downloader.
       zotero # It's actually good at archiving despite not being a researcher myself.
     ];
