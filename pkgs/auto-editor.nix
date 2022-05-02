cca026ef1095c2206aa8f024196d1d1cf3ffdf73
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-05-02 17:26:58 +0800

    auto-editor: 22w12a -> 22w17a

--- pkgs/auto-editor.nix
+++ pkgs/auto-editor.nix
@@ -1,38 +1,33 @@
 { stdenv, lib, fetchFromGitHub, python310Packages }:
 
 let
-  pillow = python310Packages.pillow.overrideAttrs (super: rec {
-    version = "9.0.1";
-    src = python310Packages.fetchPypi {
-      inherit version;
-      pname = super.pname;
-      sha256 = "sha256-bIvII4p9/a96dfXsWmY/QXP4w2flo5+H5yBJXh7tdfo=";
-    };
-  });
-
   av = python310Packages.av.overrideAttrs (super: rec {
-    version = "9.0.1";
+    version = "9.2.0";
     src = python310Packages.fetchPypi {
       inherit version;
       pname = super.pname;
-      sha256 = "sha256-IBbGImSg0Ars6w9Ati35+el2Bnyl86m9v1Mv5exTQZ8=";
+      sha256 = "sha256-8qfCJnJNf3dFs3a0WcUA2dF72NBHO36mv43bT3lXxp0=";
     };
     buildInputs = super.buildInputs ++ [ python310Packages.cython ];
+    pytestFlagsArray = super.pytestFlagsArray ++ [
+      "--deselect=tests/test_python_io.py::TestPythonIO::test_writing_to_custom_io_dash"
+      "--deselect=tests/test_python_io.py::TestPythonIO::test_writing_to_custom_io_image2"
+    ];
   });
 in python310Packages.buildPythonApplication rec {
   pname = "auto-editor";
-  version = "22w12a";
+  version = "22w17a";
   doCheck = false;
 
   src = fetchFromGitHub {
     owner = "WyattBlue";
     repo = pname;
     rev = version;
-    sha256 = "sha256-mtVL/X6mh+0Paa6yuth9DGbqewsSBopV5/VXoC0DN4M=";
+    sha256 = "sha256-zXKmmJk7QDeHFJemhabTNhcMfP+FdOvfqEkh7+Hs2z8=";
   };
 
   propagatedBuildInputs = with python310Packages;
-    [ numpy yt-dlp ] ++ [ av pillow ];
+    [ numpy yt-dlp pillow ] ++ [ av ];
 
   meta = with lib; {
     description =
