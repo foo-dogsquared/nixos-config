3b32e9d7b7e1adda4c63dfbe653eebe2bf9fdfe1
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-07-06 06:39:50 +0800

    secrets: rekey all files

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,9 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG42LafAFOeh3oYz/cm6FXes0ss59/EOCXpGsYvhpI21";
   system2 =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHjRjAddjbyoM32tQhCjj8OrnqNBsXj+5D379iryupK+";
-  systems = [ system1 system2 ];
+  system3 =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ4X7YXsEmMW3jP2dfU9l/KrF9jUZqN0sVXSvkag8VFH";
+  systems = [ system1 system2 system3 ];
 
   user1 =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMclb6WPpYRoMVqCCzQcG2XQHczB6vaIEDIHqjVsyQJi";
@@ -17,6 +19,7 @@ let
 in {
   "archive/borg-patterns".publicKeys = users ++ systems;
   "archive/borg-patterns-local".publicKeys = users ++ systems;
+  "archive/borg-ssh-key".publicKeys = systems;
   "archive/password".publicKeys = users ++ systems;
   "archive/key".publicKeys = users ++ systems;
 }
