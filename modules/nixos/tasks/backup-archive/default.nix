01bf630a9d37011fd3f0f092cbaf059df9e6d3eb
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-07-20 12:00:51 +0800

    tasks/backup-archive: organize secrets and update remote backup

--- modules/nixos/tasks/backup-archive/default.nix
+++ modules/nixos/tasks/backup-archive/default.nix
@@ -10,7 +10,7 @@ let
     doInit = true;
     encryption = {
       mode = "repokey-blake2";
-      passCommand = "cat ${config.sops.secrets.borg-password.path}";
+      passCommand = "cat ${config.sops.secrets."borg-backup/password".path}";
     };
     extraCreateArgs = lib.concatStringsSep " "
       (builtins.map (patternFile: "--patterns-from ${patternFile}") patterns);
@@ -45,12 +45,14 @@ in {
       getKey = key: {
         inherit key;
         sopsFile = lib.getSecret "backup-archive.yaml";
+        name = "borg-backup/${key}";
       }; in {
-      borg-patterns-home = getKey "borg-patterns/home";
-      borg-patterns-etc = getKey "borg-patterns/etc";
-      borg-patterns-keys = getKey "borg-patterns/keys";
-      borg-ssh-key = getKey "ssh-key";
-      borg-password = getKey "password";
+      "borg-backup/patterns/home" = getKey "borg-patterns/home";
+      "borg-backup/patterns/etc" = getKey "borg-patterns/etc";
+      "borg-backup/patterns/keys" = getKey "borg-patterns/keys";
+      "borg-backup/patterns/remote-backup" = getKey "borg-patterns/remote-backup";
+      "borg-backup/ssh-key" = getKey "ssh-key";
+      "borg-backup/password" = getKey "password";
     };
 
     fileSystems."/mnt/external-storage" = {
@@ -97,10 +99,10 @@ in {
 
     services.borgbackup.jobs = {
       local-archive = borgJobCommonSetting {
-        patterns = [
-          config.sops.secrets.borg-patterns-home.path
-          config.sops.secrets.borg-patterns-etc.path
-          config.sops.secrets.borg-patterns-keys.path
+        patterns = with config.sops; [
+          secrets."borg-backup/patterns/home".path
+          secrets."borg-backup/patterns/etc".path
+          secrets."borg-backup/patterns/keys".path
         ];
       } // {
         doInit = false;
@@ -110,10 +112,10 @@ in {
       };
 
       local-external-drive = borgJobCommonSetting {
-        patterns = [
-          config.sops.secrets.borg-patterns-home.path
-          config.sops.secrets.borg-patterns-etc.path
-          config.sops.secrets.borg-patterns-keys.path
+        patterns = with config.sops; [
+          secrets."borg-backup/patterns/home".path
+          secrets."borg-backup/patterns/etc".path
+          secrets."borg-backup/patterns/keys".path
         ];
       } // {
         doInit = false;
@@ -123,17 +125,19 @@ in {
       };
 
       remote-borgbase = borgJobCommonSetting {
-        patterns = [ config.sops.secrets.borg-patterns-home.path ];
+        patterns = with config.sops; [
+          secrets."borg-backup/patterns/remote-backup".path
+        ];
       } // {
         repo = "r6o30viv@r6o30viv.repo.borgbase.com:repo";
         startAt = "daily";
-        environment.BORG_RSH = "ssh -i ${config.sops.secrets.borg-ssh-key.path}";
+        environment.BORG_RSH = "ssh -i ${config.sops.secrets."borg-backup/ssh-key".path}";
       };
     };
 
     programs.ssh.extraConfig = ''
       Host *.repo.borgbase.com
-       IdentityFile ${config.sops.secrets.borg-ssh-key.path}
+       IdentityFile ${config.sops.secrets."borg-backup/ssh-key".path}
     '';
   };
 }
