79ca5476255fc0da77fe724bd42ba81a638a819d
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-08-07 19:01:44 +0800

    adwcustomizer: 2022-07-19 -> 2022-08-07

--- pkgs/adwcustomizer/default.nix
+++ pkgs/adwcustomizer/default.nix
@@ -1,16 +1,34 @@
-{ stdenv, lib, fetchFromGitHub, wrapGAppsHook4, meson, ninja, pkg-config, glib, desktop-file-utils, gettext, blueprint-compiler, python3Packages, appstream-glib, gtk4, libadwaita, libportal, libportal-gtk4 }:
+{ stdenv
+, lib
+, fetchFromGitHub
+, wrapGAppsHook4
+, meson
+, ninja
+, pkg-config
+, glib
+, desktop-file-utils
+, gettext
+, blueprint-compiler
+, python3Packages
+, appstream-glib
+, gtk4
+, libadwaita
+, libportal
+, libportal-gtk4
+, gobject-introspection
+}:
 
 # Not all parts of the application works with the current nixpkgs version of
 # libadwaita.
 python3Packages.buildPythonApplication rec {
   pname = "adwcustomizer";
-  version = "2022-07-19";
+  version = "2022-08-07";
 
   src = fetchFromGitHub {
     owner = "ArtyIF";
     repo = "AdwCustomizer";
-    rev = "5a6fa1b2ba63a5a8ac3861f28882c4e62f62b10b";
-    sha256 = "sha256-KwvAlcRfilu/rC6e145xMC/6I7OXsZYWlYd0GNZoYDs";
+    rev = "d20bf680672b53f8be0c046b99e704fad29e7b2d";
+    sha256 = "sha256-Js6YXPcMOaEOnfAlQ01WKDdOBCcnLOHnznEN8WxIu0s=";
   };
 
   patches = [
@@ -25,21 +43,23 @@ python3Packages.buildPythonApplication rec {
     meson
     ninja
     pkg-config
-    glib
     desktop-file-utils
     gettext
-    appstream-glib
     blueprint-compiler
     gtk4
+  ];
+
+  propagatedBuildInputs = [
+    gobject-introspection
+    appstream-glib
+    glib
     libadwaita
     libportal
     libportal-gtk4
-  ];
-
-  propagatedBuildInputs = with python3Packages; [
+  ] ++ (with python3Packages; [
     pygobject3
     anyascii
-  ];
+  ]);
 
   preFixup = ''
     makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
