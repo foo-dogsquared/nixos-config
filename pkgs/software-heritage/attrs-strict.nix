44f31c7151e2fa7f3c4064c368b06be042faf630
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-08-10 18:31:36 +0800

    attrs-strict: init at 1.0.0

---
+++ pkgs/software-heritage/attrs-strict.nix
@@ -0,0 +1,23 @@
+{ stdenv, lib, python3Packages, ... }:
+
+with python3Packages;
+buildPythonPackage rec {
+  pname = "attrs_strict";
+  version = "1.0.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-YybB5RxP0v/8ndYH3sBsMa3WTu29N6ZR6oj2Y6N8Pxg";
+  };
+
+  propagatedBuildInputs = [
+    setuptools-scm
+    attrs
+  ];
+
+  meta = with lib; {
+    homepage = "https://github.com/bloomberg/attrs-strict";
+    description = "Runtime validators for attrs";
+    license = licenses.asl20;
+  };
+}
