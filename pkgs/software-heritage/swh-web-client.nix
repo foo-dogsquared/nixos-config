54e03461ae2acc30b3bcff689e760a9968f6599b
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-08-10 18:32:49 +0800

    swh-web-client: init at 0.5.0

---
+++ pkgs/software-heritage/swh-web-client.nix
@@ -0,0 +1,30 @@
+{ lib, python3Packages, swh-core, swh-model, swh-auth, ... }:
+
+with python3Packages;
+buildPythonPackage rec {
+  pname = "swh-web-client";
+  version = "0.5.0";
+
+  src = fetchPypi {
+    inherit version;
+    pname = "swh.web.client";
+    sha256 = "sha256-TC3KMMf2lpZA9DuwViu1Osb07eT25K+LyS49jeNZVwA=";
+  };
+
+  doCheck = false;
+  propagatedBuildInputs = [
+    dateutil
+    click
+    requests
+
+    swh-core
+    swh-model
+    swh-auth
+  ];
+
+  meta = with lib; {
+    description = "Software Heritage web client";
+    homepage = "https://forge.softwareheritage.org/source/swh-web-client/";
+    license = licenses.gpl3Only;
+  };
+}
