d95175c5889d947b680544b2f98e1c9929e721fd
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-08-31 12:41:41 +0800

    auto-editor: 22w25a -> 22w32a

--- pkgs/auto-editor.nix
+++ pkgs/auto-editor.nix
@@ -1,18 +1,24 @@
-{ stdenv, lib, fetchFromGitHub, python310Packages }:
+{ stdenv, lib, ffmpeg, fetchFromGitHub, python310Packages }:
 
 python310Packages.buildPythonApplication rec {
   pname = "auto-editor";
-  version = "22w25a";
+  version = "22w32a";
   doCheck = false;
 
   src = fetchFromGitHub {
     owner = "WyattBlue";
     repo = pname;
     rev = version;
-    sha256 = "sha256-SKlAgGqowFvvenhbFiTWbVLYAB5CChQ+EdPXxsWxNgE=";
+    sha256 = "sha256-VbEr3/8PmV9mShoyv2hsc0rX8RUM39lgVk5HGe5DKYY=";
   };
 
+  postPatch = ''
+    sed ./setup.py -i -E \
+      -e "/ae-ffmpeg==1.0.0/d"
+  '';
+
   propagatedBuildInputs = with python310Packages; [ numpy yt-dlp av pillow ];
+  runtimeDependencies = [ ffmpeg ];
 
   meta = with lib; {
     description =
