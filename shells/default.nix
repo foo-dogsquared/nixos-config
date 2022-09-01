881d015674d45ccd9de46813ce3a039bda5400ad
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-09-01 09:56:57 +0800

    shells/nix: init devshell
    Though, it has been added by accident (whoops). It's for real this time.
    :)

--- shells/default.nix
+++ shells/default.nix
@@ -4,6 +4,7 @@ with pkgs; {
   flatpak = callPackage ./flatpak.nix { };
   gnu = callPackage ./gnu.nix { };
   gnome = callPackage ./gnome.nix { };
+  nix = callPackage ./nix.nix { };
   hugo = callPackage ./hugo.nix { };
   rust = callPackage ./rust.nix { };
   tic-80 = callPackage ./tic-80.nix { };
--- shells/nix.nix
+++ shells/nix.nix
@@ -1,8 +1,13 @@
-{ mkShell, lib, nixfmt, rnix-lsp }:
+# For usual Nix projects such as overlays, package repositories, and whatnot.
+# Also, it's fun to have a file named `nix.nix`.
+{ mkShell, lib, nixfmt, jq, rnix-lsp, rnix-hashes, nix-tree }:
 
 mkShell {
   packages = [
-    nixfmt
-    rnix-lsp
+    jq # It will use some JSON with its lockfile so better be ready to use this.
+    nixfmt # Ideally, it would be nicer if the codebase has their preferred formatter but we'll go with the most common formatter(?).
+    rnix-hashes # Quick utility for converting hashes.
+    rnix-lsp # Make your editing experience nicer with a nice language server.
+    nix-tree # Suprisingly nice exploration tool for your packages in the store directory.
   ];
 }
