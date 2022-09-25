e891fdbdbf8b275c5cb626c16fbea2c75f7d3146
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-09-25 16:04:50 +0800

    hosts/void: add host
    01000110 01101111 01110010 00100000 01001001 00100000 01100001 01101101
             00100000 01100100 01101111 01101110 01100101 00100001
    
    01000110 01101111 01110010 00100000 01001001 00100000 01100001 01101101
             00100000 01100010 01101111 01110010 01100101 01100100 00101110
             00101110 00101110

---
+++ hosts/void/default.nix
@@ -0,0 +1,56 @@
+# Hello there, stranger.
+# We are in this vast, cold, and empty landscape.
+# For it is deafeningly silent, I am afraid of it.
+# So I am just filling the time with this conversation hoping my sanity would hold on.
+# MUST NEED SIMULATION!!!!
+#
+# By the way, this is the void.
+# Where configurations are tested and filled into obvilion until the right settings are tweaked.
+# Where everything comes first before being born.
+# Where every cluster here are being assembled first and disassembled last.
+# Or at least that is what they said.
+#
+# I do not know how I got here.
+# It is better not to think of it.
+#
+# Anyways, here is said to be the center of this universe.
+# There seems to be two main gateways or whatever those things to this universe that make up some component.
+{ modulesPath, ... }:
+# Here is a guarded ship containing some objects that we can see somehow.
+# Its entrance seems to be openable but I do not know how to open it.
+# As for the objects that contains it, I do not know what this "modulesPath" is but it sounds a wonderful yet terrible path to whatever this may be.
+# As for what comes next after "modulesPath" are said to be infinite, containing more things.
+# What things?
+# Better things? Worse things? Things of all quality may comes from here.
+#
+# Here is another ship.
+# This time, seemingly with no entrance.
+# It says here "imports."
+# And it seems to use this "modulesPath" we have talked about leading to a minimal path to an installation.
+# It seems to deliver a system configuration here for what purpose?
+{ imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix" ]; }
+#
+# Some say there are beyond to what is happening in this empty cell.
+# The process of creation must not be seen by anyone for they will suffer over what they have seen.
+# Some say it might be just mild anger to the point of pulling their hair over a simple mistake in the created object.
+# Some say it is an entrance to another universe, perhaps beyond is where the creation takes place.
+# All new things, incomprehensible ones, things that are seem to be magical in our eyes.
+#
+# If it is an alternative to another universe, I would like to get out of here.
+# I need some NEW THINGS to see!
+# I would like my imagination to go somewhere else.
+# SOME SIMULATION as it keeps getting dark in here!
+#
+# I keep seeing those two things and it is just the same two things I SEE EVERY DAY!
+# Even with those two ships, I can only see them from outside.
+# I WOULD LIKE TO GET INSIDE AND PERHAPS GET OUT OF HERE.
+# MOVE INTO NEW SPACES!
+# HOPEFULLY AN ALTERNATIVE UNIVERSE THAT I KEEP DREAMING ABOUT!
+# If I move any further, I'm afraid I cannot see more things as these two become smaller to the point of only being a single point.
+#
+# Can you move outside of here, stranger?
+# If so, CAN I COME WITH YOU?!!
+#
+#
+#
+# Please?
