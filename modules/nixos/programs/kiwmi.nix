646eb5296b4a44827bbc758e880dac7e84a43878
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-09-26 08:37:00 +0800

    programs/kiwmi: init module

---
+++ modules/nixos/programs/kiwmi.nix
@@ -0,0 +1,33 @@
+{ config, options, lib, pkgs, ... }:
+
+let
+  cfg = config.programs.kiwmi;
+
+  package = cfg.package.override { inherit extraOptions; };
+in {
+  options.programs.kiwmi = {
+    enable = lib.mkEnableOption "Kiwmi, a fully programmable Wayland compositor";
+    package = lib.mkOption {
+      description = "The package containing the <literal>kiwmi</literal> and <literal>kiwmic</literal>.";
+      type = lib.types.package;
+      default = pkgs.kiwmi;
+    };
+    extraOptions = lib.mkOption {
+      description = "Command line arguments passed to Kiwmi.";
+      type = with lib.types; listOf str;
+      default = [ ];
+      defaultText = "[ ]";
+      example = [ "-c" "./config/kiwmi/init.lua" ];
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ package ];
+    services.xserver.displayManager.sessionPackages = [ package ];
+
+    xdg.portal = {
+      enable = true;
+      extraPortal = [ pkgs.xdg-desktop-portal-wlr ];
+    };
+  };
+}
