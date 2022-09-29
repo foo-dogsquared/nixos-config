6cd97ed0fe1e0703483310f94846d0b82d899f2d
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-09-29 11:34:07 +0800

    devShells.default: add language servers for this project

--- shell.nix
+++ shell.nix
@@ -1,5 +1,17 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  packages = with pkgs; [ asciidoctor age git jq nixpkgs-fmt rnix-lsp sops ];
+  packages = with pkgs; [
+    asciidoctor
+    age
+    git
+    jq
+    nixpkgs-fmt
+    sops
+
+    # Language servers for various parts of the config that uses a language.
+    sumneko-lua-language-server
+    pyright
+    rnix-lsp
+  ];
 }
