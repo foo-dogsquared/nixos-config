487d0717d54f23511a0f2b4ec88140d0e4d50ea4
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-11-30 08:31:56 +0800

    users/admin: change password

--- users/nixos/admin/default.nix
+++ users/nixos/admin/default.nix
@@ -5,7 +5,7 @@
 {
   users.users.admin = {
     description = "The administrator account for the servers.";
-    hashedPassword = "$6$QEHdYhTige1mhCyT$yIfecQpV0PZJNxdxVLiRAk.0UxYXYxASlzzyBoYqEkbRx2fsaF81JKaw.Alb.ENKY.5UKkGdcV8H4bPAdJIwR1";
+    hashedPassword = "$6$KXZD6NvjtSkle/id$ECs7zIwDBOlQiFACsyot1gyjKG9UWMlUdRknVujE9efpHMQGx7.YZWyJ0VkV0ja0BPzeF/EzTu6n4EEF5ZHPD0";
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     useDefaultShell = true;
