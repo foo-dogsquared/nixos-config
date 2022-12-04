2cc7faf2e72611a957a9a0359a406b4b52d568f6
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-12-05 07:17:57 +0800

    users/admin: add trust to more applications

--- users/nixos/admin/default.nix
+++ users/nixos/admin/default.nix
@@ -2,8 +2,11 @@
 # installations with deploy-rs.
 { config, lib, pkgs, ... }:
 
+let
+  name = "admin";
+in
 {
-  users.users.admin = {
+  users.users.${name} = {
     description = "The administrator account for the servers.";
     hashedPassword = "$6$KXZD6NvjtSkle/id$ECs7zIwDBOlQiFACsyot1gyjKG9UWMlUdRknVujE9efpHMQGx7.YZWyJ0VkV0ja0BPzeF/EzTu6n4EEF5ZHPD0";
     isNormalUser = true;
@@ -15,5 +18,11 @@
     ];
   };
 
-  nix.settings.trusted-users = [ "admin" ];
+  # This is also a trusted user for the Nix daemon.
+  nix.settings.trusted-users = [ name ];
+
+  # Allow the user to easily enter into several services such as the database
+  # services to allowing some debugging.
+  services.postgresql.ensureUsers = [ { inherit name; } ];
+  services.mysql.ensureUsers = [ { inherit name; } ];
 }
