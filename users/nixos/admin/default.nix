57078178f37531cf15196015b797f892be7b42e0
Gabriel Arazas <foodogsquared@foodogsquared.one>
2022-12-13 09:19:48 +0800

    users/admin: make the account passwordless

--- users/nixos/admin/default.nix
+++ users/nixos/admin/default.nix
@@ -1,5 +1,4 @@
-# This is the main account for servers. It is also used for managing the
-# installations with deploy-rs.
+# Mainly used for managing the installations with deploy-rs.
 { config, lib, pkgs, ... }:
 
 let
@@ -8,7 +7,6 @@ in
 {
   users.users.${name} = {
     description = "The administrator account for the servers.";
-    hashedPassword = "$6$KXZD6NvjtSkle/id$ECs7zIwDBOlQiFACsyot1gyjKG9UWMlUdRknVujE9efpHMQGx7.YZWyJ0VkV0ja0BPzeF/EzTu6n4EEF5ZHPD0";
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     useDefaultShell = true;
@@ -17,6 +15,17 @@ in
       ../../../hosts/ni/files/ssh-key.pub
     ];
   };
+
+  # We're going passwordless, baybee!
+  security.sudo.extraRules = [{
+    users = [ name ];
+    options = [ "NOPASSWD" ];
+  }];
+
+  security.doas.extraRules = [{
+    users = [ name ];
+    noPass = true;
+  }];
 
   # This is also a trusted user for the Nix daemon.
   nix.settings.trusted-users = [ name ];
