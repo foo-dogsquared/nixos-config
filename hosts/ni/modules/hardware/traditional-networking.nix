8369ea414556e4a0b8b313ed7a9ce366cb8b9e22
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-03-09 11:43:47 +0800

    hosts/ni: switch to iwd for wireless network configuration

--- hosts/ni/modules/hardware/traditional-networking.nix
+++ hosts/ni/modules/hardware/traditional-networking.nix
@@ -10,6 +10,26 @@
     interfaces.enp1s0.useDHCP = true;
     interfaces.wlp2s0.useDHCP = true;
 
+    # The simpler WiFi manager.
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        General = {
+          EnableNetworkConfiguration = true;
+          UseDefaultInterface = true;
+          ControlPortOverNL80211 = true;
+        };
+
+        Network = {
+          AutoConnect = true;
+          NameResolvingService = "systemd";
+        };
+      };
+    };
+
+    # Set the NetworkManager backend to iwd for workflows that use it.
+    networkmanager.wifi.backend = "iwd";
+
     bonds.bond0 = {
       driverOptions = {
         miimon = "100";
