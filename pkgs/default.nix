695f115a59b852803015b9f51bbc95d101773388
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-04-22 17:11:05 +0800

    furtherance: remove package
    It is available in nixpkgs now.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -15,7 +15,6 @@ let
       clidle = callPackage ./clidle.nix { };
       domterm = callPackage ./domterm { };
       freerct = callPackage ./freerct.nix { };
-      furtherance = callPackage ./furtherance { };
       distant = callPackage ./distant.nix { };
       gol-c = callPackage ./gol-c.nix { };
       gnome-search-provider-recoll =
