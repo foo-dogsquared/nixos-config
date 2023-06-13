753699869e6a867314d80a69d1ad8d690619f8d1
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-06-13 13:33:36 +0800

    hosts/plover: update nameserver list

--- hosts/plover/default.nix
+++ hosts/plover/default.nix
@@ -55,6 +55,20 @@ in
     };
   };
 
+  networking.nameservers = [
+    # dns.google
+    "8.8.8.8"
+    "8.8.4.4"
+    "2001:4860:4860::8888"
+    "2001:4860:4860::8844"
+
+    # cloudflare-dns.com
+    "1.1.1.1"
+    "1.0.0.1"
+    "2606:4700:4700::1111"
+    "2606:4700:4700::1001"
+  ];
+
   services.fail2ban = {
     ignoreIP = [
       # VPN clients.
