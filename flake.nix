23e00d0472c4b8949e1ff8633c539264ff96a25f
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-06-19 12:26:25 +0800

    config: remove foo-dogsquared's binary cache
    It is pretty outdated and the CI didn't able to build the packages for
    some time now (that I'm lazy to fix). Not to mention, some of them are
    being upstreamed to nixpkgs so the need to use this is low. Also, it
    causes the build to have some errors.

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
 
   nixConfig = {
     extra-substituters =
-      "https://nix-community.cachix.org https://foo-dogsquared.cachix.org";
+      "https://nix-community.cachix.org";
     extra-trusted-public-keys =
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= foo-dogsquared.cachix.org-1:/2fmqn/gLGvCs5EDeQmqwtus02TUmGy0ZlAEXqRE70E=";
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
   };
 
   inputs = {
@@ -175,11 +175,9 @@
           # Set several binary caches.
           substituters = [
             "https://nix-community.cachix.org"
-            "https://foo-dogsquared.cachix.org"
           ];
           trusted-public-keys = [
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "foo-dogsquared.cachix.org-1:/2fmqn/gLGvCs5EDeQmqwtus02TUmGy0ZlAEXqRE70E="
           ];
 
           # Sane config for the package manager.
