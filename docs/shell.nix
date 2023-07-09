fc9b172c7d75aa7b9f93d62ed093b0231d9ad357
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-07-09 13:18:13 +0800

    docs/site: add Asciidoctor templates for custom wrapper

--- docs/shell.nix
+++ docs/shell.nix
@@ -3,6 +3,10 @@
 with pkgs;
 
 let
+  asciidoctorWrapper = writeShellScriptBin "asciidoctor" ''
+    ${lib.getBin gems}/bin/asciidoctor -T ${./assets/templates/asciidoctor}
+  '';
+
   gems = bundlerEnv {
     name = "nixos-config-project-docs";
     ruby = ruby_3_1;
@@ -15,6 +19,7 @@ mkShell {
     gems.wrappedRuby
     bundix
 
+    asciidoctorWrapper
     hugo
     go
     nodePackages.prettier
