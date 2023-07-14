c6fe84155bc8d8df47804030cbcd716fd00ea6a9
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-07-14 22:25:58 +0800

    gnome-shell-extension-burn-my-windows: remove package
    It is available on nixpkgs.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -17,7 +17,6 @@ let
       gol-c = callPackage ./gol-c.nix { };
       gnome-search-provider-recoll =
         callPackage ./gnome-search-provider-recoll.nix { };
-      gnome-shell-extension-burn-my-windows = callPackage ./gnome-shell-extension-burn-my-windows { };
       gnome-shell-extension-fly-pie =
         callPackage ./gnome-shell-extension-fly-pie.nix { };
       guile-config = callPackage ./guile-config.nix { };
