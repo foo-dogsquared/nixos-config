4cae5c709b2aa59d9e1c6b042c47a5e5b6911751
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-07-14 14:44:16 +0800

    shells/hugo: update NodeJS 16 with latest version
    This is to prevent periodic update with NodeJS versions being insecure
    at some point.

--- shells/hugo.nix
+++ shells/hugo.nix
@@ -1,11 +1,11 @@
 # My usual toolchain for developing Hugo projects.
-{ mkShell, hugo, git, go, nodejs-16_x }:
+{ mkShell, hugo, git, go, nodejs_latest }:
 
 mkShell {
   packages = [
     hugo # The main tool.
     go # I might use Go modules which requires the Golang runtime.
     git # VCS of my choice.
-    nodejs-16_x # The supported NodeJS version.
+    nodejs_latest # The supported NodeJS version.
   ];
 }
