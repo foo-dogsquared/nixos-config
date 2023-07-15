6eb80bb910f2a7ec99913c708ea54cdb1c4f0fbd
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-07-15 15:43:22 +0800

    distant: 0.16.4 -> 0.20.0

--- pkgs/distant.nix
+++ pkgs/distant.nix
@@ -1,25 +1,36 @@
-{ stdenv, lib, rustPlatform, fetchFromGitHub, perl, pkg-config, openssl }:
+{ stdenv
+, lib
+, rustPlatform
+, fetchFromGitHub
+, pkg-config
+, openssl
+}:
 
 rustPlatform.buildRustPackage rec {
-  version = "0.16.4";
+  version = "0.20.0";
   pname = "distant";
 
   src = fetchFromGitHub {
     owner = "chipsenkbeil";
     repo = "distant";
     rev = "v${version}";
-    sha256 = "sha256-lCiTlyzp+q3NnwrILQZYM60fmbjfWFWYAy1rn7HqP54=";
+    hash = "sha256-DcnleJUAeYg3GSLZljC3gO9ihiFz04dzT/ddMnypr48=";
   };
-  cargoSha256 = "sha256-0oCSHstuZ/K+cerOa8xEHett8diVmDTjzvo+uLuRtWo=";
+  cargoHash = "sha256-7MNNdm4b9u5YNX04nBtKcrw+phUlpzIXo0tJVfcgb40=";
+
+  # Too many tests failing for now so we'll have to disable them. Much of the
+  # failed tests require a home directory and network access.
+  doCheck = false;
 
   # We'll just tell to use the system's openssl to build openssl-sys.
-  OPENSSL_NO_VENDOR = 1;
+  env.OPENSSL_NO_VENDOR = 1;
   nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ perl openssl ];
+  buildInputs = [ openssl ];
 
   meta = with lib; {
     description = "Remotely edit files and run programs";
     homepage = "https://github.com/chipsenkbeil/distant";
     license = lib.licenses.mit;
+    maintainers = with maintainers; [ foo-dogsquared ];
   };
 }
