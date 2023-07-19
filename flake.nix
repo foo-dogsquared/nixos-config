3c60989da4ac46695ad6f7b2f7e2b420a4c82301
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-07-19 15:35:46 +0800

    config: add foodogsquared's binary cache
    YAY! It's usable again. :D

--- flake.nix
+++ flake.nix
@@ -186,9 +186,11 @@
           # Set several binary caches.
           substituters = [
             "https://nix-community.cachix.org"
+            "https://foo-dogsquared.cachix.org"
           ];
           trusted-public-keys = [
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "foo-dogsquared.cachix.org-1:/2fmqn/gLGvCs5EDeQmqwtus02TUmGy0ZlAEXqRE70E="
           ];
 
           # Sane config for the package manager.
