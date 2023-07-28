101f3771a1e3f1fdff69411f33a33d36b138c96d
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-07-28 08:53:32 +0800

    hosts/plover: update Wezterm mux server configuration

--- hosts/plover/config/wezterm/config.lua
+++ hosts/plover/config/wezterm/config.lua
@@ -3,5 +3,6 @@ return {
     pem_private_key = os.getenv("CREDENTIALS_DIRECTORY") .. "/key.pem",
     pem_cert = os.getenv("CREDENTIALS_DIRECTORY") .. "/cert.pem",
     pem_ca = os.getenv("CREDENTIALS_DIRECTORY") .. "/fullchain.pem",
+    bind_address = "@host_address@:@port@",
   }
 }
--- hosts/plover/modules/services/wezterm-mux-server.nix
+++ hosts/plover/modules/services/wezterm-mux-server.nix
@@ -3,23 +3,35 @@
 # We're setting up Wezterm mux server with TLS domains.
 let
   weztermDomain = "mux.${config.networking.domain}";
+
+  configFile = pkgs.substituteAll {
+    src = ../../config/wezterm/config.lua;
+    domain = weztermDomain;
+    port = 9801;
+  };
 in
 {
   services.wezterm-mux-server = {
     enable = true;
-    configFile = ../../config/wezterm/config.lua;
+    inherit configFile;
+    user = "plover";
+    group = "users";
   };
 
-  systemd.services.wezterm-mux-server.serviceConfig = {
-    LoadCredential = let
-      certDir = config.security.acme.certs."${weztermDomain}".directory;
-      credentialCertPath = path: "${path}:${certDir}/${path}";
-    in
-    [
-      (credentialCertPath "key.pem")
-      (credentialCertPath "cert.pem")
-      (credentialCertPath "fullchain.pem")
-    ];
+  systemd.services.wezterm-mux-server = {
+    requires = [ "acme-finished-${weztermDomain}.target" ];
+    environment.WEZTERM_LOG = "info";
+    serviceConfig = {
+      LoadCredential = let
+        certDir = config.security.acme.certs."${weztermDomain}".directory;
+        credentialCertPath = path: "${path}:${certDir}/${path}";
+      in
+      [
+        (credentialCertPath "key.pem")
+        (credentialCertPath "cert.pem")
+        (credentialCertPath "fullchain.pem")
+      ];
+    };
   };
 
   security.acme.certs."${weztermDomain}".postRun = ''
