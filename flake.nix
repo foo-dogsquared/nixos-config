b97cc4415af427cfe735144bedc2a2370b0611b8
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-07-30 14:17:04 +0800

    flake.nix: update list of caches for flake config

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
 
   nixConfig = {
     extra-substituters =
-      "https://nix-community.cachix.org";
+      "https://nix-community.cachix.org https://foo-dogsquared.cachix.org";
     extra-trusted-public-keys =
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= foo-dogsquared.cachix.org-1:/2fmqn/gLGvCs5EDeQmqwtus02TUmGy0ZlAEXqRE70E=";
   };
 
   inputs = {
