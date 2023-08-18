7099ed8645eb28af24fd2a44be43b3d5516ef8c8
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-08-18 17:21:17 +0800

    swh.swh-auth: 0.7.1 -> 0.7.2

--- pkgs/software-heritage/swh-auth.nix
+++ pkgs/software-heritage/swh-auth.nix
@@ -4,12 +4,12 @@ with python3Packages;
 
 buildPythonPackage rec {
   pname = "swh-auth";
-  version = "0.7.1";
+  version = "0.7.2";
 
   src = fetchPypi {
     inherit version;
     pname = "swh.auth";
-    sha256 = "sha256-tmHyDSUDIWLNRTgPquyYaeIg2wZdYMJWpK9Gy8h1B6k=";
+    sha256 = "sha256-f0++AuyJggoc19kPA/7UChbFjF/EoR+FztF00r5csLo=";
   };
 
   # Tests require network access.
@@ -22,11 +22,11 @@ buildPythonPackage rec {
     click
     pyyaml
     (python-keycloak.overrideAttrs (final: prev: rec {
-      version = "2.16.1";
+      version = "3.3.0";
       src = pkgs.fetchPypi {
         inherit version;
         pname = "python_keycloak";
-        hash = "sha256-LyJwC274wWcoSoLCNzb2/ryQW9CrhZgdyhIXGt82Z68=";
+        hash = "sha256-zIaBJvU1qk8yDcnqsk5GrzgcE7zIjZsHAbBCk+p1zSQ=";
       };
       propagatedBuildInputs = prev.propagatedBuildInputs ++ [ setuptools deprecation ];
     }))
