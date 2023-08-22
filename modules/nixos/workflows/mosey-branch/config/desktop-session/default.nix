df192650306b89f2dd843388207b8b22471a0c7f
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-08-22 10:12:23 +0800

    workflows/mosey-branch: remove unnecessary symbolic link

--- modules/nixos/workflows/mosey-branch/config/desktop-session/default.nix
+++ modules/nixos/workflows/mosey-branch/config/desktop-session/default.nix
@@ -39,11 +39,6 @@ stdenv.mkDerivation rec {
       --prefix PATH : "${lib.makeBinPath [ gnome.gnome-session ]}"
   '';
 
-  postFixup = ''
-    mkdir -p $out/share/wayland-sessions
-    ln -s $out/share/applications/${prefix}desktop $out/share/wayland-sessions
-  '';
-
   meta = with lib; {
     description = "Custom desktop files for the custom desktop environment";
     license = licenses.gpl3Plus;
