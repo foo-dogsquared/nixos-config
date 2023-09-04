8c02a0db68c75d27ddfc153fc7d2aac7bcdb2be1
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-09-04 14:13:05 +0800

    profiles/dev: add gum into `extras` submodule

--- modules/home-manager/profiles/dev.nix
+++ modules/home-manager/profiles/dev.nix
@@ -161,6 +161,7 @@ in {
       home.packages = with pkgs; [
         act # Test your CI without embarrassing yourself repeatedly pushing into GitHub repos.
         github-cli # So you don't have to use much of GitHub on the site, I guess.
+        gum # The fancy shell script toolkit.
         hut # So you don't have to see much of Sourcehut's brutalist design, I guess.
         hyperfine # Making sure your apps are not just fine but REEEEEEAAAAALY fine.
         irssi # Communicate in the terminal like a normal person.
