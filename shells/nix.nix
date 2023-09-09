991a7f4fabeddf36a2b02222439f1d32c06873ec
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-09-09 20:14:37 +0800

    shells/nix: update devshell env

--- shells/nix.nix
+++ shells/nix.nix
@@ -2,21 +2,25 @@
 # Also, it's fun to have a file named `nix.nix`.
 { mkShell
 , lib
+, deadnix
 , jq
+, nil
 , nix-tree
 , nixfmt
+, nixpkgs-hammering
 , nurl
 , rnix-hashes
-, rnix-lsp
 }:
 
 mkShell {
   packages = [
+    deadnix # Search for the dead.
     jq # It will use some JSON with its lockfile so better be ready to use this.
-    nixfmt # Ideally, it would be nicer if the codebase has their preferred formatter but we'll go with the most common formatter(?).
-    rnix-hashes # Quick utility for converting hashes.
-    rnix-lsp # Make your editing experience nicer with a nice language server.
+    nil # Language server.
     nix-tree # Suprisingly nice exploration tool for your packages in the store directory.
+    nixfmt # Ideally, it would be nicer if the codebase has their preferred formatter but we'll go with the most common formatter(?).
+    nixpkgs-hammering # Beat nixpkgs derivations up to shape.
     nurl # Nice way to catch up with some things.
+    rnix-hashes # Quick utility for converting hashes.
   ];
 }
