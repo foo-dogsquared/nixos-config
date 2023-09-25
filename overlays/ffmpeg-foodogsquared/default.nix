c3f095295bac45b98c9ff9ba3a6b16f97129684d
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-09-25 22:22:32 +0800

    overlays/ffmpeg-foodogsquared: fix gltransitions filter
    Still broken but it can be fixed later.

--- overlays/ffmpeg-foodogsquared/default.nix
+++ overlays/ffmpeg-foodogsquared/default.nix
@@ -5,25 +5,43 @@ let
     owner = "transitive-bullshit";
     repo = "ffmpeg-gl-transition";
     rev = "3639b521aafb30b185de281f94560f298a22d420";
-    hash = "";
+    hash = "sha256-py6NVXw3giiRAcVRzsgxU8aKJZInWrubIUT2vOhfuco=";
+    name = "ffmpeg-gltransition";
   };
 
-  ffmpegShadertoyFilter = prev.fetchFromGitLab {
+  ffmpegShadertoy = prev.fetchFromGitLab {
     owner = "kriwkrow";
     repo = "ffmpeg_shadertoy_filter";
     rev = "eb297df10a104cae2d4ef3f70188d1e84f104532";
-    hash = "";
+    hash = "sha256-Qy5sZgNF/0uNCosj2NZEvyssXU9ln6ZsDjnt/orpt1k=";
+    name = "ffmpeg-shadertoy";
   };
 in
 {
   ffmpeg-foodogsquared = prev.ffmpeg-full.overrideAttrs (finalAttrs: prevAttrs: {
     pname = "ffmpeg-foodogsquared";
+    srcs = [
+      prevAttrs.src
+      ffmpegGLTransitions
+      ffmpegShadertoy
+    ];
+    buildInputs = prevAttrs.buildInputs ++ (with prev; [
+      libGLU
+      glew
+    ]);
+    sourceRoot = ".";
     patches = prevAttrs.patches ++ [
       ./add-custom-filters.patch
+      ./update-ffmpeg-opengltransition.patch
     ];
-    postPatch = prevAttrs.postPatch + ''
-      cp ${ffmpegGLTransitions}/vf_gltransition.c $src/libavfilter
-      cp ${ffmpegShadertoyFilter}/vf_shadertoy.c $src/libavfilter
+    postUnpack = ''
+      cd ./${ffmpegGLTransitions.name}
+      cd ../
+
+      cp --no-preserve=mode ./${ffmpegGLTransitions.name}/vf_gltransition.c ./ffmpeg/libavfilter
+      cp --no-preserve=mode ./${ffmpegShadertoy.name}/vf_shadertoy.c ./ffmpeg/libavfilter
+
+      cd ffmpeg
     '';
   });
 }
