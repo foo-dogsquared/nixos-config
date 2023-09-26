d3a83233dfa2c44f6e4bb5b072a69a23fb8688a4
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-09-26 22:34:14 +0800

    default.nix: update with custom overlays

--- default.nix
+++ default.nix
@@ -7,6 +7,8 @@ in
 {
   lib = import ./lib { lib = pkgs.lib; };
   modules = lib'.importModules (lib'.filesToAttr ./modules/nixos);
-  overlays.foo-dogsquared-pkgs = final: prev: import ./pkgs { pkgs = prev; };
+  overlays = import ./overlays // {
+    foo-dogsquared-pkgs = final: prev: import ./pkgs { pkgs = prev; };
+  };
   hmModules = lib'.importModules (lib'.filesToAttr ./modules/home-manager);
 } // (import ./pkgs { inherit pkgs; })
