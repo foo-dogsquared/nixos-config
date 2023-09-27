3c68db65dc52432fe22e2395b7aeaf66caa4d1ff
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-09-27 10:51:07 +0800

    shells/latex: update inputs

--- shells/latex.nix
+++ shells/latex.nix
@@ -1,8 +1,12 @@
 # Ripperootskees for the space.
-{ mkShell, texlive }:
+{ mkShell
+, texlive
+, texlab
+}:
 
 mkShell {
   packages = [
-    texlive.combined.scheme-full
+    texlive.combined.scheme-full # RIP YOUR DISK SPACE!
+    texlab # Otherwise, here's a tool to easily write your (en)grave(ing).
   ];
 }
