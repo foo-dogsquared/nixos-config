d34ad343876f43ee4ef10a73704fcd4b3f632dfe
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-09-27 10:50:58 +0800

    shells/hugo: update inputs

--- shells/hugo.nix
+++ shells/hugo.nix
@@ -1,5 +1,11 @@
 # My usual toolchain for developing Hugo projects.
-{ mkShell, hugo, git, go, nodejs_latest }:
+{ mkShell
+, hugo
+, git
+, go
+, nodejs_latest
+, imagemagick
+}:
 
 mkShell {
   packages = [
@@ -7,5 +13,6 @@ mkShell {
     go # I might use Go modules which requires the Golang runtime.
     git # VCS of my choice.
     nodejs_latest # The supported NodeJS version.
+    imagemagick # Everyman's image processing framework.
   ];
 }
