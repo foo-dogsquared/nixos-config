fc5eb2b0e25990e799b478ecff07421751bbcf01
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-10-05 12:27:59 +0800

    hosts/plover: update Wezterm mux server config

--- hosts/plover/config/wezterm/config.lua
+++ hosts/plover/config/wezterm/config.lua
@@ -1,8 +1,10 @@
 return {
 	tls_servers = {
-		pem_private_key = os.getenv("CREDENTIALS_DIRECTORY") .. "/key.pem",
-		pem_cert = os.getenv("CREDENTIALS_DIRECTORY") .. "/cert.pem",
-		pem_ca = os.getenv("CREDENTIALS_DIRECTORY") .. "/fullchain.pem",
-		bind_address = "@host_address@:@port@",
+        {
+            pem_private_key = os.getenv("CREDENTIALS_DIRECTORY") .. "/key.pem",
+            pem_cert = os.getenv("CREDENTIALS_DIRECTORY") .. "/cert.pem",
+            pem_ca = os.getenv("CREDENTIALS_DIRECTORY") .. "/fullchain.pem",
+            bind_address = "@domain@:@port@",
+        },
 	},
 }
