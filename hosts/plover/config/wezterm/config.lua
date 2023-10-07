a6fcc6eec63fdd5df12530c06db3fe69c06d4404
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-10-08 03:29:06 +0800

    hosts/plover: update Wezterm mux server config
    It's not fully working but we'll get there.

--- hosts/plover/config/wezterm/config.lua
+++ hosts/plover/config/wezterm/config.lua
@@ -1,10 +1,12 @@
 return {
 	tls_servers = {
+        -- These are expected to be imported through systemd LoadCredentials
+        -- directive.
         {
             pem_private_key = os.getenv("CREDENTIALS_DIRECTORY") .. "/key.pem",
             pem_cert = os.getenv("CREDENTIALS_DIRECTORY") .. "/cert.pem",
             pem_ca = os.getenv("CREDENTIALS_DIRECTORY") .. "/fullchain.pem",
-            bind_address = "@domain@:@port@",
+            bind_address = "@listen_address@",
         },
 	},
 }
--- hosts/plover/modules/services/wezterm-mux-server.nix
+++ hosts/plover/modules/services/wezterm-mux-server.nix
@@ -3,20 +3,21 @@
 # We're setting up Wezterm mux server with TLS domains.
 let
   weztermDomain = "mux.${config.networking.domain}";
+  port = 9801;
+  listenAddress = "127.0.0.1:${builtins.toString port}";
 
   configFile = pkgs.substituteAll {
     src = ../../config/wezterm/config.lua;
-    domain = weztermDomain;
-    port = 9801;
+    listen_address = listenAddress;
   };
 in
 {
   services.wezterm-mux-server = {
     enable = true;
     inherit configFile;
-    user = "plover";
-    group = "users";
   };
+
+  networking.firewall.allowedTCPPorts = [ port ];
 
   systemd.services.wezterm-mux-server = {
     requires = [ "acme-finished-${weztermDomain}.target" ];
