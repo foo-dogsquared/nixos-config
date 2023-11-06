a19c6530b28ee5a0996d8e7f6c198481586f06c9
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-11-06 17:05:46 +0800

    templates/sample-nixos-template: update default NixOS config

--- templates/sample-nixos-template/hosts/desktop/default.nix
+++ templates/sample-nixos-template/hosts/desktop/default.nix
@@ -1,5 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
+  fileSystems."/".label = "root";
+  fileSystems."/boot".label = "boot";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   system.stateVersion = "23.05";
 }
