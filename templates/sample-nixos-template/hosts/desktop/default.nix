75e393dd024667c33122ca21d07258d41eb4df83
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-11-11 17:52:36 +0800

    templates/sample-nixos-template: update desktop template

--- templates/sample-nixos-template/hosts/desktop/default.nix
+++ templates/sample-nixos-template/hosts/desktop/default.nix
@@ -8,4 +8,20 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   system.stateVersion = "23.05";
+
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "yes";
+  };
+
+  users.users.nixos = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "audio" ];
+    initialHashedPassword = "";
+  };
+
+  nix = {
+    package = pkgs.nixUnstable;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
 }
