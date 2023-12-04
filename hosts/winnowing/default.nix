44463c2b28c3ba2a72e0e5dffd12b6df3bc889f3
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-12-04 17:57:55 +0800

    hosts/winnowing: init

---
+++ hosts/winnowing/default.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [
+    "${modulesPath}/profiles/minimal.nix"
+
+    (lib.mapHomeManagerUser "foo-dogsquared" {
+      extraGroups = [
+        "adbusers"
+        "wheel"
+        "audio"
+        "docker"
+        "podman"
+        "networkmanager"
+        "wireshark"
+      ];
+      hashedPassword =
+        "$y$j9T$UFzEKZZZrmbJ05CTY8QAW0$X2RD4m.xswyJlXZC6AlmmuubPaWPQZg/Q1LDgHpXHx1";
+      isNormalUser = true;
+      createHome = true;
+      home = "/home/foo-dogsquared";
+      description = "Gabriel Arazas";
+    })
+  ];
+
+  wsl = {
+    enable = true;
+    defaultUser = "winnow";
+    nativeSystemd = true;
+  };
+
+  programs.bash.loginShellInit = "nixos-wsl-welcome";
+
+  # Setting the development environment mainly for container-related work.
+  profiles.dev.enable = true;
+  profiles.dev.virtualization.enable = true;
+}
