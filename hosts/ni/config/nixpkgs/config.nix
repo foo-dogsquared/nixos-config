c57695bd7c6abd95cad2750f7033664f33101eaa
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-12-17 20:15:58 +0800

    hosts/ni: remove unnecessary nixpkgs config

--- hosts/ni/config/nixpkgs/config.nix
+++ hosts/ni/config/nixpkgs/config.nix
@@ -1,10 +1,3 @@
 {
   allowUnfree = true;
-
-  # These are wrapper options for Firefox.
-  firefox = {
-    enableBrowserpass = true;
-    enableTridactylNative = true;
-    enableFxCastBridge = true;
-  };
 }
