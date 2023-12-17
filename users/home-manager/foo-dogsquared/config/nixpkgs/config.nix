3b7b2d711300ea9661b72045cf9b50c24ffa8710
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-12-17 20:15:44 +0800

    users/foo-dogsquared: remove unnecessary nixpkgs config

--- users/home-manager/foo-dogsquared/config/nixpkgs/config.nix
+++ users/home-manager/foo-dogsquared/config/nixpkgs/config.nix
@@ -2,11 +2,4 @@
   allowUnfree = true;
   allowInsecure = true;
   allowBroken = true;
-
-  # These are wrapper options for Firefox.
-  firefox = {
-    enableBrowserpass = true;
-    enableTridactylNative = true;
-    enableFxCastBridge = true;
-  };
 }
