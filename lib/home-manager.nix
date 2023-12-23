cf5ad9ce3c5a14479a00ea09c9744895cc3d775f
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-12-23 18:40:12 +0800

    lib/home-manager: add `hasOSModuleEnabled`

--- lib/home-manager.nix
+++ lib/home-manager.nix
@@ -1,7 +1,7 @@
 # Custom libraries for home-manager library.
 { lib }:
 
-{
+rec {
   /*
     Checks if there is the `osConfig` attribute and get the attribute path from
     its system configuration.
@@ -16,4 +16,15 @@
     # The default value when `attrPath` is missing.
     default:
     attrs ? osConfig && lib.attrByPath attrPath default attrs;
+
+  /*
+    A quick function to check if the optional system module is enabled.
+  */
+  hasOSModuleEnabled =
+    # The configuration attribute set of the home-manager configuration.
+    attrs:
+
+    # A list of strings representing the attribute path.
+    attrPath:
+    getOSConfigPath attrs attrPath false;
 }
