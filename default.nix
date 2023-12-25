3839be59e3a8f97e6609bf8c732b86c430aad7b1
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-12-25 20:03:42 +0800

    default.nix: add overlays.default

--- default.nix
+++ default.nix
@@ -6,8 +6,9 @@ in
 {
   lib = import ./lib { lib = pkgs.lib; };
   modules.default.imports = import ./modules/nixos { inherit lib; };
-  overlays = import ./overlays // {
+  overlays = import ./overlays // rec {
     foo-dogsquared-pkgs = final: prev: import ./pkgs { pkgs = prev; };
+    default = foo-dogsquared-pkgs;
   };
   hmModules.default.imports = import ./modules/home-manager { inherit lib; };
 } // (import ./pkgs { inherit pkgs; })
