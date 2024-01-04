27b21464b4559752098f1592e3bc699dcfd41e86
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-01-04 20:06:35 +0800

    users/alice: init home-manager config

---
+++ users/home-manager/alice/default.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+
+{
+  profiles = {
+    dev = {
+      enable = true;
+      shell.enable = true;
+    };
+    editors.neovim.enable = true;
+  };
+
+  home.sessionVariables = {
+    MANPAGER = "nvim +Man!";
+    EDITOR = "nvim";
+  };
+
+  # My user shell of choice because I'm not a hipster.
+  programs.bash = {
+    enable = true;
+    historyControl = [ "erasedups" "ignoredups" "ignorespace" ];
+    historyIgnore = [
+      "cd"
+      "exit"
+      "lf"
+      "ls"
+      "nvim"
+    ];
+  };
+}
