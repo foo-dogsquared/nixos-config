5e37db23617b0e053405b1f45ad7221219325b4b
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-01-17 17:38:53 +0800

    firefox-addons: add "Sourcegraph for Firefox" extension

--- pkgs/firefox-addons/default.nix
+++ pkgs/firefox-addons/default.nix
@@ -86,6 +86,26 @@
         platforms = platforms.all;
       };
     };
+    "sourcegraph-for-firefox" = buildFirefoxXpiAddon {
+      pname = "sourcegraph-for-firefox";
+      version = "23.4.14.1343";
+      addonId = "sourcegraph-for-firefox@sourcegraph.com";
+      url = "https://addons.mozilla.org/firefox/downloads/file/4097469/sourcegraph_for_firefox-23.4.14.1343.xpi";
+      sha256 = "fa02236d75a82a7c47dabd0272b77dd9a74e8069563415a7b8b2b9d37c36d20b";
+      meta = with lib;
+      {
+        description = "Adds code intelligence to GitHub, GitLab, Bitbucket Server, and Phabricator: hovers, definitions, references. Supports 20+ languages.";
+        mozPermissions = [
+          "activeTab"
+          "storage"
+          "contextMenus"
+          "https://github.com/*"
+          "https://sourcegraph.com/*"
+          "<all_urls>"
+        ];
+        platforms = platforms.all;
+      };
+    };
     "tineye-reverse-image-search" = buildFirefoxXpiAddon {
       pname = "tineye-reverse-image-search";
       version = "2.0.4";
