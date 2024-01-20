cdfbab649017c54c63f0a2e2c93c7ff6883f5dae
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-01-20 17:17:01 +0800

    flake: fix templates

--- configs/flake-parts/templates.nix
+++ configs/flake-parts/templates.nix
@@ -3,19 +3,19 @@
   flake.templates = {
     default = inputs.self.templates.basic-devshell;
     basic-devshell = {
-      path = ../templates/basic-devshell;
+      path = ../../templates/basic-devshell;
       description = "Basic development shell template";
     };
     basic-overlay-flake = {
-      path = ../templates/basic-overlay-flake;
+      path = ../../templates/basic-overlay-flake;
       description = "Basic overlay as a flake";
     };
     sample-nixos-template = {
-      path = ../templates/sample-nixos-template;
+      path = ../../templates/sample-nixos-template;
       description = "Simple sample Nix flake with NixOS and home-manager";
     };
     local-ruby-nix = {
-      path = ../templates/local-ruby-nix;
+      path = ../../templates/local-ruby-nix;
       description = "Local Ruby app development with ruby-nix";
     };
   };
