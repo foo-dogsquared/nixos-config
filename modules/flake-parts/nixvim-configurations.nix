2a8b8d9b08f2e676455f6d05d0bd2132b1ba3102
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-01-25 19:09:57 +0800

    flake-parts/nixvim-configurations: init

---
+++ modules/flake-parts/nixvim-configurations.nix
@@ -0,0 +1,49 @@
+{ config, lib, flake-parts-lib, ... }:
+
+let
+  inherit (flake-parts-lib) mkSubmoduleOptions mkPerSystemOption;
+in
+{
+  options = {
+    flake = mkSubmoduleOptions {
+      nixvimConfigurations = lib.mkOption {
+        type = with lib.types; lazyAttrsOf (attrsOf package);
+        default = { };
+        description = ''
+          An attribute set of per-system builds of
+          [NixVim](https://github.com/nix-community/nixvim) configurations
+          similar to `packages` flake output.
+        '';
+      };
+    };
+
+    perSystem = mkPerSystemOption {
+      options = {
+        nixvimConfigurations = lib.mkOption {
+          type = with lib.types; attrsOf package;
+          default = { };
+          description = ''
+            An attribute set of per-system builds of
+            [NixVim](https://github.com/nix-community/nixvim) configurations
+            similar to `packages` flake output.
+          '';
+        };
+      };
+    };
+  };
+
+  config = {
+    flake.nixvimConfigurations =
+      lib.mapAttrs
+        (k: v: v.nixvimConfigurations)
+        (lib.filterAttrs
+          (k: v: v.nixvimConfigurations != { })
+          config.allSystems
+        );
+
+    perInput = system: flake:
+      lib.optionalAttrs (flake ? nixvimConfigurations.${system}) {
+        nixvimConfigurations = flake.nixvimConfigurations.${system};
+      };
+  };
+}
