26822d655dcfe4642dcab5f6361be589f1e1bf33
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-01-26 16:47:30 +0800

    nixvim/plugins/firenvim: init

---
+++ modules/nixvim/plugins/firenvim.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, helpers, ... }:
+
+let
+  cfg = config.plugins.firenvim;
+in
+{
+  options.plugins.firenvim = {
+    enable = lib.mkEnableOption "Firenvim";
+    package = helpers.mkPackageOption "firenvim" pkgs.vimPlugins.firenvim;
+    extraConfig = lib.mkOption {
+      type = with lib.types; attrsOf anything;
+      default = {};
+      example = {
+        globalSettings = { alt = "all"; };
+        localSettings = {
+          "\".*\"" = {
+            cmdline = "nvim";
+            content = "text";
+            priority = 0;
+            selector = "textarea";
+            takeover = "always";
+          };
+        };
+      };
+      description = ''
+        Extra configuration options for Firenvim.
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    extraPlugins = [ cfg.package ];
+
+    globals.firenvim_config = cfg.extraConfig;
+  };
+}
