1c08463334afc4c0696694680909bc2c6e6b7dde
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-01-28 11:45:09 +0800

    flake-parts/setups: refactor Nixvim instance option module

--- modules/flake-parts/setups/shared/nixvim-instance-options.nix
+++ modules/flake-parts/setups/shared/nixvim-instance-options.nix
@@ -1,32 +1,22 @@
 { lib, ... }: {
-  options.nixvim = lib.mkOption {
-    type = lib.types.submodule {
-      options = {
-        instance = lib.mkOption {
-          type = with lib.types; nullOr str;
-          default = null;
-          example = "fiesta";
-          description = ''
-            The name of the NixVim configuration from
-            {option}`setups.nixvim.configs.<name>` to be included as part
-            of the NixOS system.
-          '';
-        };
-
-        additionalModules = lib.mkOption {
-          type = with lib.types; listOf raw;
-          default = [ ];
-          description = ''
-            A list of additional NixVim modules to be included.
-          '';
-        };
-      };
+  options.nixvim = {
+    instance = lib.mkOption {
+      type = with lib.types; nullOr str;
+      default = null;
+      example = "fiesta";
+      description = ''
+        The name of the NixVim configuration from
+        {option}`setups.nixvim.configs.<name>` to be included as part
+        of the NixOS system.
+      '';
     };
-    default = { };
-    description = ''
-      An optional NixVim inclusion for the environment. Take note, this
-      will override whatever Neovim configuration from your environment so
-      be sure to only use this if you have none.
-    '';
+
+    additionalModules = lib.mkOption {
+      type = with lib.types; listOf raw;
+      default = [ ];
+      description = ''
+        A list of additional NixVim modules to be included.
+      '';
+    };
   };
 }
