9729cd30052799cb2a5fafbf2ef16f719eaa20ad
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-01-29 22:38:44 +0800

    nixvimConfigs/fiesta: configure general settings

--- configs/nixvim/fiesta/default.nix
+++ configs/nixvim/fiesta/default.nix
@@ -3,19 +3,40 @@
 {
   imports = [ ./modules ];
 
-  nixvimConfigs.fiesta.setups = {
-    snippets.enable = true;
-    completion.enable = true;
-    treesitter.enable = true;
-    debugging.enable = true;
-    desktop-utils.enable = true;
-    ui.enable = true;
+  config = {
+    nixvimConfigs.fiesta.setups = {
+      snippets.enable = true;
+      ui.enable = true;
+      completion.enable = true;
+      treesitter.enable = true;
+      debugging.enable = true;
+      desktop-utils.enable = true;
+      note-taking.enable = true;
+    };
+
+    # Some general settings.
+    globals = {
+      mapleader = " ";
+      maplocalleader = ",";
+      syntax = true;
+    };
+
+    options = {
+      encoding = "utf-8";
+      completeopt = [ "menuone" "noselect" ];
+    };
+
+    keymaps = [
+      {
+        mode = "i";
+        key = "jk";
+        action = "<Esc>";
+      }
+    ];
+
+    plugins.nvim-autopairs.enable = true;
+    extraPlugins = with pkgs; [
+      decker
+    ];
   };
-
-  plugins.neorg.enable = true;
-  plugins.nvim-autopairs.enable = true;
-
-  extraPlugins = with pkgs; [
-    decker
-  ];
 }
