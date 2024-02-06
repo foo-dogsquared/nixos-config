cc488eb39159f7be66ce6359565003e3930cea9b
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-02-07 07:54:37 +0800

    nixvim/plugins/nvim-config-local: init

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./keyunmaps.nix
     ./plugins/firenvim.nix
+    ./plugins/nvim-config-local.nix
   ];
 }
---
+++ modules/nixvim/plugins/nvim-config-local.nix
@@ -0,0 +1,43 @@
+{ config, lib, pkgs, helpers, ... }:
+
+let
+  cfg = config.plugins.nvim-config-local;
+in
+{
+  options.plugins.nvim-config-local =
+    helpers.neovim-plugin.extraOptionsOptions // {
+      enable = lib.mkEnableOption "nvim-config-local";
+
+      package = helpers.mkPackageOption "nvim-config-local" pkgs.vimPlugins.nvim-config-local;
+
+      configFiles = lib.mkOption {
+        type = with lib.types; listOf str;
+        default = [ ".nvim.lua" ".nvimrc" ".exrc" ];
+        example = [ "config/nvim.lua" ];
+        description = ''
+          A list of patterns to load (includes Lua configurations).
+        '';
+      };
+
+      autocommandsCreate = helpers.defaultNullOpts.mkBool true "Create autocommands for sourcing local files.";
+      commandsCreate = helpers.defaultNullOpts.mkBool true "Create user commands for nvim-config-local.";
+      lookupParents = helpers.defaultNullOpts.mkBool false "Enable lookup in parent directories when sourcing local configs.";
+    };
+
+  config =
+    let
+      setupOptions = {
+        config_files = cfg.configFiles;
+        autocommands_create = cfg.autocommandsCreate;
+        commands_create = cfg.commandsCreate;
+        lookup_parents = cfg.lookupParents;
+      } // cfg.extraOptions;
+    in
+    lib.mkIf cfg.enable {
+      extraPlugins = [ cfg.package ];
+
+      extraConfigLua = ''
+        require("config-local").setup(${helpers.toLuaConfig setupOptions})
+      '';
+    };
+}
