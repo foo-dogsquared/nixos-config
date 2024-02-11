a1aa211ed4e8f7be66e2baf6db67b9052dda9a5a
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-02-11 15:20:09 +0800

    users/foo-dogsquared: set custom Tinted Theming schemes

--- configs/home-manager/foo-dogsquared/default.nix
+++ configs/home-manager/foo-dogsquared/default.nix
@@ -72,4 +72,56 @@
       type = "git";
     };
   };
+
+  # My own colorscheme just set somewhere. For now, this is manually set, we'll
+  # have to import this with `lib.importYAML` or something similar.
+  tinted-theming.schemes.bark-on-a-tree = {
+    name = "Bark on a tree";
+    author = "Gabriel Arazas";
+    description = "Rusty theme resembling forestry inspired from Nord theme.";
+    variant = "dark";
+    palette = {
+      base00 = "2b221f";
+      base01 = "412c26";
+      base02 = "5c362c";
+      base03 = "a45b43";
+      base04 = "e1bcb2";
+      base05 = "f5ecea";
+      base06 = "fefefe";
+      base07 = "eb8a65";
+      base08 = "d03e68";
+      base09 = "df937a";
+      base0A = "afa644";
+      base0B = "85b26e";
+      base0C = "eb914a";
+      base0D = "c67f62";
+      base0E = "8b7ab9";
+      base0F = "7f3F83";
+    };
+  };
+
+  tinted-theming.schemes.albino-bark-on-a-tree = {
+    name = "Albino bark on a tree";
+    author = "Gabriel Arazas";
+    variant = "light";
+    description = "Bright rusty theme resembling forestry inspired from Nord theme.";
+    palette = {
+      base00 = "f0f0f0";
+      base01 = "e1e3e2";
+      base02 = "dacec7";
+      base03 = "9d5c4c";
+      base04 = "54352c";
+      base05 = "392c26";
+      base06 = "2b220f";
+      base07 = "cb6d48";
+      base08 = "b52b52";
+      base09 = "d56f17";
+      base0A = "b0a52e";
+      base0B = "5c963e";
+      base0C = "e46403";
+      base0D = "954c2f";
+      base0E = "6751a5";
+      base0F = "55195a";
+    };
+  };
 }
