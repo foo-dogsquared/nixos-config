911476c37c4b8afc5b8cbdc0fab16826f7c4ba91
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-02-23 14:47:49 +0800

    bahaghari: move into subprojects directory
    This'll make it easier to transfer to a dedicated repo in case.

--- subprojects/bahaghari/lib/tinted-theming.nix
+++ subprojects/bahaghari/lib/tinted-theming.nix
@@ -1,6 +1,16 @@
 { pkgs, lib }:
 
 {
+  # TODO: Return a derivation containing all of the template output from the
+  # given schemes.
+  generateOutputFromSchemes = schemes: template:
+    pkgs.runCommand "generate-templates" { } ''
+    '';
+
+  # TODO: Return a Nix object to generate a Tinted Theming color scheme from an
+  # image.
+  generateScheme = image: { };
+
   # A very naive implementation of checking if a Tinted Theming scheme is a
   # Base16 scheme.
   isBase16 = palette:
