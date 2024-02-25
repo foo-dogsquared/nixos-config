df345c8b51176d7005cfcd1a92d38f9c7b1d1e5f
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-02-25 17:12:49 +0800

    bahaghari/tests/lib: add tests for tinted-theming subset

--- subprojects/bahaghari/tests/lib/tinted-theming/default.nix
+++ subprojects/bahaghari/tests/lib/tinted-theming/default.nix
@@ -2,7 +2,9 @@
 
 let
   sampleBase16Scheme = lib.trivial.importYAML ./sample-base16-scheme.yml;
+  sampleBase16Scheme' = lib.trivial.importYAML ./sample-base16-scheme-with-missing-colors.yml;
   sampleBase24Scheme = lib.trivial.importYAML ./sample-base24-scheme.yml;
+  sampleBase24Scheme' = lib.trivial.importYAML ./sample-base24-scheme-with-missing-colors.yml;
 in
 pkgs.lib.runTests {
   testIsBase16 = {
@@ -10,8 +12,18 @@ pkgs.lib.runTests {
     expected = true;
   };
 
+  testIsNotBase16 = {
+    expr = lib.tinted-theming.isBase16 sampleBase16Scheme'.palette;
+    expected = false;
+  };
+
   testIsBase24 = {
     expr = lib.tinted-theming.isBase24 sampleBase24Scheme.palette;
     expected = true;
+  };
+
+  testIsNotBase24 = {
+    expr = lib.tinted-theming.isBase24 sampleBase24Scheme'.palette;
+    expected = false;
   };
 }
