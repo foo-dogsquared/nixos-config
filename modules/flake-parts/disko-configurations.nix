04f309850842cbb30f7ac8e78e2435ad8174a8f4
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-02-27 20:54:47 +0800

    flake-parts/disko-configurations: init

---
+++ modules/flake-parts/disko-configurations.nix
@@ -0,0 +1,18 @@
+{ lib, flake-parts-lib, inputs, ... }:
+
+{
+  options = {
+    flake = flake-parts-lib.mkSubmoduleOptions {
+      diskoConfigurations = lib.mkOption {
+        type = with lib.types; attrsOf (inputs.disko.lib.topLevel);
+        default = { };
+        description = ''
+          A set of [disko](https://github.com/nix-community/disko)
+          configurations readily available as part of the flake output to be
+          used by {program}`disko`. Could be useful as backup initialization
+          scripts for individual storage drives.
+        '';
+      };
+    };
+  };
+}
