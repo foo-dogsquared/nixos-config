54104720b5e9e61445c000a953bd4151d1e347c9
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-02-27 20:53:27 +0800

    nixos-generators/install-iso-graphical: init

---
+++ modules/nixos-generators/install-iso-graphical.nix
@@ -0,0 +1,18 @@
+# A custom nixos-generator module to set as a graphical installation with a
+# graphical installer profile. Useful for hosts that can be used both as a
+# persistent live installer or as a graphical ISO. Based from the original
+# install-iso format.
+{ lib, modulesPath, ... }:
+
+{
+  imports = [
+    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares.nix"
+  ];
+
+  # override installation-cd-base and enable wpa and sshd start at boot
+  systemd.services.wpa_supplicant.wantedBy = lib.mkForce ["multi-user.target"];
+  systemd.services.sshd.wantedBy = lib.mkForce ["multi-user.target"];
+
+  formatAttr = "graphicalIsoImage";
+  fileExtension = ".iso";
+}
