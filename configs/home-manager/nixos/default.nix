a0c73c3901fc01ffe31a1fac9eb358e5962337e4
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-02-28 21:17:27 +0800

    users/nixos: init

---
+++ configs/home-manager/nixos/default.nix
@@ -0,0 +1,33 @@
+# This is mostly just the same as the Alice user except it used for installers.
+# It should be niiiiiiiiiice enough for the usual cases, you feel me.
+{ config, lib, pkgs, ... }:
+
+{
+  suites = {
+    dev = {
+      enable = true;
+      shell.enable = true;
+    };
+    editors.neovim.enable = true;
+  };
+
+  home.sessionVariables = {
+    MANPAGER = "nvim +Man!";
+    EDITOR = "nvim";
+  };
+
+  # My user shell of choice because I'm not a hipster.
+  programs.bash = {
+    enable = true;
+    historyControl = [ "erasedups" "ignoredups" "ignorespace" ];
+    historyIgnore = [
+      "cd"
+      "exit"
+      "lf"
+      "ls"
+      "nvim"
+    ];
+  };
+
+  home.stateVersion = "23.11";
+}
