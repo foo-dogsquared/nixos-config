21ce62aa006ecb9d5a55d94de003e4a6baab2362
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-03-02 18:18:44 +0800

    hosts/graphical-installer: update config

--- configs/nixos/graphical-installer/default.nix
+++ configs/nixos/graphical-installer/default.nix
@@ -1,18 +1,34 @@
-{ lib, config, pkgs, foodogsquaredLib, foodogsquaredModulesPath, ... }:
+{ lib
+, config
+, pkgs
+, foodogsquaredLib
+, foodogsquaredUtils
+, foodogsquaredModulesPath
+
+, ... }:
 
 # Since this will be exported as an installer ISO, you'll have to keep in mind
 # about the added imports from nixos-generators. In this case, it simply adds
 # the NixOS installation CD profile.
 {
   imports = [
-    "${foodogsquaredModulesPath}/profiles/installer"
+    "${foodogsquaredModulesPath}/profiles/installer.nix"
 
-    (foodogsquaredLib.mapHomeManager "nixos" { })
+    (foodogsquaredUtils.mapHomeManagerUser "nixos" { })
   ];
 
   config = lib.mkMerge [
     {
+      # Use the systemd-boot EFI boot loader.
+      boot.loader.systemd-boot = {
+        enable = true;
+        netbootxyz.enable = true;
+      };
+      boot.loader.efi.canTouchEfiVariables = true;
       boot.kernelPackages = pkgs.linuxPackages_6_6;
+
+      # We'll make NetworkManager manage all of them networks.
+      networking.wireless.enable = false;
 
       # Use my desktop environment configuration without the apps just to make the
       # closure size smaller.
