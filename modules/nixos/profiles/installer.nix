4a19b2004e5f3104c785574d94baf71614825c9e
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-03-03 10:26:19 +0800

    nixos/profiles/installer: update

--- modules/nixos/profiles/installer.nix
+++ modules/nixos/profiles/installer.nix
@@ -1,7 +1,15 @@
-# A dedicated profile for installers with some niceties in it.
-{ pkgs, ... }:
+# A dedicated profile for installers with some niceties in it. This is also
+# used for persistent live installers.
+{ pkgs, modulesPath, ... }:
 
 {
+  imports = [
+    "${modulesPath}/profiles/all-hardware.nix"
+    "${modulesPath}/profiles/base.nix"
+    "${modulesPath}/profiles/installation-device.nix"
+  ];
+
+  # Include some modern niceties.
   environment.systemPackages = with pkgs; [
     ripgrep
     git
