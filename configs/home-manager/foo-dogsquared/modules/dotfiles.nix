0c13466cb534871659eb4449e665f3e3b8e18b10
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-03-07 18:44:09 +0800

    users/foo-dogsquared/dotfiles: fix conditionals

--- configs/home-manager/foo-dogsquared/modules/dotfiles.nix
+++ configs/home-manager/foo-dogsquared/modules/dotfiles.nix
@@ -24,18 +24,22 @@ in
       home.sessionPath = [
         "${config.home.mutableFile.${dotfiles}.path}/bin"
       ];
-
-      xdg.configFile = {
-        doom.source =
-          lib.mkIf userCfg.programs.doom-emacs.enable (getDotfiles "emacs");
-        kitty.source =
-          lib.mkIf userCfg.setups.development.enable (getDotfiles "kitty");
-        nyxt.source =
-          lib.mkIf userCfg.programs.browsers.misc.enable (getDotfiles "nyxt");
-        wezterm.source =
-          lib.mkIf userCfg.setups.development.enable (getDotfiles "wezterm");
-      };
     }
+
+    (lib.mkIf (userCfg.programs.doom-emacs.enable) {
+      xdg.configFile.doom.source = getDotfiles "emacs";
+    })
+
+    (lib.mkIf (userCfg.setups.development.enable) {
+      xdg.configFile = {
+        kitty.source = getDotfiles "emacs";
+        wezterm.source = getDotfiles "wezterm";
+      };
+    })
+
+    (lib.mkIf (userCfg.programs.browsers.misc.enable) {
+      xdg.configFile.nyxt.source = getDotfiles "nyxt";
+    })
 
     (lib.mkIf (!userCfg.programs.nixvim.enable) {
       xdg.configFile.nvim.source = getDotfiles "nvim";
