a04da2a0ec14287eda7ffaefda38c484c745520e
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-07-08 21:13:32 +0800

    wrapper-manager-fds/tests: update test for env subset

--- subprojects/wrapper-manager-fds/tests/lib/env/default.nix
+++ subprojects/wrapper-manager-fds/tests/lib/env/default.nix
@@ -1,6 +1,32 @@
 { pkgs, lib, self }:
 
 lib.runTests {
+  testsEvaluateSampleConfiguration = {
+    expr =
+      let
+        sampleConf = self.env.eval {
+          inherit pkgs;
+          modules = [ ./wrapper-neofetch.nix ];
+          specialArgs.yourMomName = "Joe Mama";
+        };
+      in
+        lib.isDerivation sampleConf.config.build.toplevel;
+    expected = true;
+  };
+
+  testsEvaluateSampleConfiguration2 = {
+    expr =
+      let
+        sampleConf = self.env.eval {
+          inherit pkgs;
+          modules = [ ./wrapper-fastfetch.nix ];
+          specialArgs.yourMomName = "Joe Mama";
+        };
+      in
+        lib.isDerivation sampleConf.config.build.toplevel;
+    expected = true;
+  };
+
   testsBuildSampleConfiguration = {
     expr =
       let
