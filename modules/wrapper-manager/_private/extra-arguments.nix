7d50020b75f1fddfd6ca2631b0e91e0ee232a103
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-07-10 15:51:23 +0800

    wrapper-manager: init extra module arguments

---
+++ modules/wrapper-manager/_private/extra-arguments.nix
@@ -0,0 +1,12 @@
+{ pkgs, lib, ... }:
+
+let
+  foodogsquaredLib = import ../../../lib { inherit pkgs; };
+in
+{
+  _module.args.foodogsquaredLib =
+    foodogsquaredLib.extend (final: prev: {
+      wrapper-manager =
+        import ../../../lib/env-specific/wrapper-manager.nix { inherit pkgs lib; self = final; };
+    });
+}
