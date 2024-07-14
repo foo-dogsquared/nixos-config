7de7807cda3aeede3a2e7d7538691248a6434426
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-07-14 11:13:08 +0800

    wrapper-manager-fds: put more things in flake

--- subprojects/wrapper-manager-fds/flake.nix
+++ subprojects/wrapper-manager-fds/flake.nix
@@ -1,4 +1,38 @@
 {
   description = "wrapper-manager-fds flake";
-  outputs = { ... }: import ./. { };
+  outputs = { ... }: let
+    sources = import ./npins;
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    eachSystem = systems: f:
+      let
+        # Merge together the outputs for all systems.
+        op = attrs: system:
+          let
+            ret = f system;
+            op = attrs: key: attrs //
+                {
+                  ${key} = (attrs.${key} or { })
+                    // { ${system} = ret.${key}; };
+                }
+            ;
+          in
+          builtins.foldl' op attrs (builtins.attrNames ret);
+      in
+      builtins.foldl' op { }
+        (systems
+         ++ # add the current system if --impure is used
+            (if builtins?currentSystem then
+               if builtins.elem builtins.currentSystem systems
+               then []
+               else [ builtins.currentSystem ]
+             else
+               []));
+  in import ./. { } // (eachSystem systems (system: let
+    pkgs = import sources.nixos-unstable { inherit system; };
+    tests = import ./tests { inherit pkgs; };
+  in {
+    devShells.default = import ./shell.nix { inherit pkgs; };
+
+    checks.wrapperManagerLibrarySetPkg = tests.libTestPkg;
+  }));
 }
