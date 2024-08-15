dfb16a9f0b8b9842e6d62e9d846f18fc192a3050
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-08-15 12:50:51 +0800

    nixvim/plugins/firenvim: remove
    There is an upstream module of it now with the same settings so it is
    unnecessary.

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -2,7 +2,6 @@
   imports = [
     ./keyunmaps.nix
     ./plugins/dressing-nvim.nix
-    ./plugins/firenvim.nix
     ./plugins/lush-nvim.nix
     ./plugins/legendary-nvim.nix
     ./plugins/nvim-config-local.nix
