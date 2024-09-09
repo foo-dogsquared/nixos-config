f6ac08e3dc185803b566ab957c85150159cc0e37
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-09-09 12:44:39 +0800

    users/foo-dogsquared: update custom homepage grid layout

--- configs/home-manager/foo-dogsquared/files/homepage/package.nix
+++ configs/home-manager/foo-dogsquared/files/homepage/package.nix
@@ -5,7 +5,7 @@
 
 buildHugoSite {
   pname = "foodogsquared-hm-startpage";
-  version = "0.1.0";
+  version = "0.2.0";
   src = lib.cleanSource ./.;
 
   vendorHash = "sha256-Mi61QK1yKWIneZ+i79fpJqP9ew5r5vnv7ptr9YGq0Uk=";
