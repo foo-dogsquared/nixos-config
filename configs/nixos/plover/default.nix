6e26946312adfe2b665404a5f9d7b7bdba92c610
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-09-19 21:27:22 +0800

    hosts/plover: update state variables and services
    This is just made in advanced for the upcoming config updates of each
    services.

--- configs/nixos/plover/default.nix
+++ configs/nixos/plover/default.nix
@@ -32,11 +32,41 @@
     fail2ban.enable = true;
 
     # The self-hosted services.
-    atuin.enable = true;
-    gitea.enable = true;
     grafana.enable = true;
-    vaultwarden.enable = true;
-    wireguard.enable = true;
+    tailscale.enable = true;
+  };
+
+  state.network = {
+    ipv4 = lib.mkDefault "65.109.224.213";
+    ipv6 = lib.mkDefault "2a01:4f9:c012:607a::1";
+
+    interfaces = {
+      lan = {
+        ipv4 = "10.0.0.2";
+        ipv6 = "";
+      };
+    };
+
+    secondaryNameservers = [
+      # ns1.first-ns.de
+      "213.239.242.238"
+      "2a01:4f8:0:a101::a:1"
+
+      # robotns2.second-ns.de
+      "213.133.105.6"
+      "2a01:4f8:d0a:2004::2"
+
+      # robotns3.second-ns.com
+      "193.47.99.3"
+      "2001:67c:192c::add:a3"
+    ];
+  };
+
+  state.paths = {
+    dataDir = "/var/lib";
+    cacheDir = "/var/cache";
+    logDir = "/var/log";
+    runtimeDir = "/run";
   };
 
   # Offline SSH!?!
@@ -61,7 +91,7 @@
     email = "admin+acme@foodogsquared.one";
     dnsProvider = "rfc2136";
     dnsResolver = "1.1.1.1";
-    credentialsFile = config.sops.secrets."lego/env".path;
+    credentialsFile = config.sops.secrets."lego/env".path or "/var/lib/secrets/acme.env";
   };
 
   # Enable generating new DH params.
