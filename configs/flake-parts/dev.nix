2d97eb963881dc7a54b71f6f2d8f5bede401b85c
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-10-28 09:56:08 +0800

    flake: update devContainers attribute

--- configs/flake-parts/dev.nix
+++ configs/flake-parts/dev.nix
@@ -8,23 +8,17 @@
 
   perSystem = { config, lib, pkgs, ... }: {
     apps = {
-      run-workflow-with-vm =
-        let
-          inputsArgs = lib.mapAttrsToList
-            (name: source:
-              let
-                name' = if (name == "self") then "config" else name;
-              in
-              "'${name'}=${source}'")
-            inputs;
-          script = pkgs.callPackage ../../apps/run-workflow-with-vm {
-            inputs = inputsArgs;
-          };
-        in
-        {
-          type = "app";
-          program = "${script}/bin/run-workflow-with-vm";
+      run-workflow-with-vm = let
+        inputsArgs = lib.mapAttrsToList (name: source:
+          let name' = if (name == "self") then "config" else name;
+          in "'${name'}=${source}'") inputs;
+        script = pkgs.callPackage ../../apps/run-workflow-with-vm {
+          inputs = inputsArgs;
         };
+      in {
+        type = "app";
+        program = "${script}/bin/run-workflow-with-vm";
+      };
     };
 
     # No amount of formatters will make this codebase nicer but it sure does
@@ -37,22 +31,21 @@
     devShells = import ../../shells { inherit pkgs; } // {
       default = import ../../shell.nix {
         inherit pkgs;
-        extraPackages = with pkgs; [
-          # Mozilla addons-specific tooling. Unfortunately, only available with
-          # flakes-based setups.
-          nur.repos.rycee.mozilla-addons-to-nix
-        ];
+        extraPackages = with pkgs;
+          [
+            # Mozilla addons-specific tooling. Unfortunately, only available with
+            # flakes-based setups.
+            nur.repos.rycee.mozilla-addons-to-nix
+          ];
       };
       website = import ../../docs/website/shell.nix { inherit pkgs; };
     };
 
     # Packages that are meant to be consumed inside of a development
     # environment.
-    devPackages = {
-      inherit (import ../../docs { inherit pkgs; }) website;
-    };
+    devPackages = { inherit (import ../../docs { inherit pkgs; }) website; };
 
     # All of the typical devcontainers to be used.
-    devcontainers = import ../../devcontainers { inherit pkgs; };
+    devContainers = import ../../devcontainers { inherit pkgs; };
   };
 }
