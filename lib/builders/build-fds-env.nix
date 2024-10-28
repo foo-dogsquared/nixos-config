f1699cc3376c73fba44b0f40d4dbe7c920931dfc
Gabriel Arazas <foodogsquared@foodogsquared.one>
2024-10-28 09:55:30 +0800

    lib/builders/build-fds-env: add default Bash prompt

--- lib/builders/build-fds-env.nix
+++ lib/builders/build-fds-env.nix
@@ -1,8 +1,23 @@
-{ buildEnv, extendedStdenv }:
+{ lib, writeTextDir, buildEnv, extendedStdenv, direnv, coreutils }:
 
 { paths ? [ ], pathsToLink ? [ ], ... }@args:
 
-buildEnv (args // {
-  paths = extendedStdenv ++ paths;
+let
+  bashProfile = writeTextDir "/etc/bashrc" ''
+    # This should only be applied to interactive shells.
+    [[ $- == *i* ]] || return
+
+    if [[ -n "$PS1" ]]; then
+      shopt -s checkwinsize
+      set +h
+
+      PS1="\h $ "
+
+      eval "$(${lib.getExe' direnv "direnv"} hook bash)"
+      eval "$(${lib.getExe' coreutils "dircolors"} --sh)"
+    fi
+  '';
+in buildEnv (args // {
+  paths = extendedStdenv ++ paths ++ [ bashProfile ];
   pathsToLink = [ "/bin" "/etc" "/share" "/lib" "/libexec" ] ++ pathsToLink;
 })
