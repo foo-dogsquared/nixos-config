0692053a9f38974bb81a4bcbfa432c31e3989c62
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-03-01 17:28:01 +0800

    hosts/plover: update and format Gitea repos on Terraform

--- configs/nixos/plover/files/gitea/main.tf
+++ configs/nixos/plover/files/gitea/main.tf
@@ -13,64 +13,76 @@ provider "gitea" {
 }
 
 resource "gitea_user" "foodogsquared" {
-  username = "foodogsquared"
+  username   = "foodogsquared"
   login_name = "foodogsquared"
-  password = var.gitea_foodogsquared_password
-  email = "foodogsquared@foodogsquared.one"
-  admin = true
-  active = true
-  full_name = "Gabriel Arazas"
-  location = "Inside of your house"
+  password   = var.gitea_foodogsquared_password
+  email      = "foodogsquared@foodogsquared.one"
+  admin      = true
+  active     = true
+  full_name  = "Gabriel Arazas"
+  location   = "Inside of your house"
 }
 
 resource "gitea_public_key" "foodogsquared" {
-  title = "main public key"
+  title    = "main public key"
   username = gitea_user.foodogsquared.username
-  key = file("../../../../home-manager/foo-dogsquared/files/ssh-key.pub")
+  key      = file("../../../../home-manager/foo-dogsquared/files/ssh-key.pub")
 }
 
 resource "gitea_repository" "personal_projects_from_github" {
   for_each = tomap({
     hugo-theme-more-contentful = {
-      website = "https://foo-dogsquared.github.io/hugo-theme-more-contentful"
+      website  = "https://foo-dogsquared.github.io/hugo-theme-more-contentful"
       interval = null
     }
     hugo-theme-contentful = {
-      website = "https://foo-dogsquared.github.io/hugo-theme-contentful"
+      website  = "https://foo-dogsquared.github.io/hugo-theme-contentful"
       interval = null
     }
     ansible-playbooks = {
-      website = null
+      website  = null
       interval = null
     }
     dotfiles = {
-      website = null
+      website  = null
       interval = null
     }
     wiki = {
-      website = "https://wiki.foodogsquared.one"
+      website  = "https://wiki.foodogsquared.one"
       interval = "1h"
     }
     asciidoctor-foodogsquared-extensions = {
-      website = null
+      website  = null
       interval = null
     }
     website = {
-      website = "https://foodogsquared.one"
+      website  = "https://foodogsquared.one"
       interval = "1h"
     }
     nixos-config = {
-      website = "https://foo-dogsquared.github.io/nixos-config"
+      website  = "https://foo-dogsquared.github.io/nixos-config"
+      interval = null
+    }
+    nix-module-wrapper-manager-fds = {
+      website  = "https://foo-dogsquared.github.io/nix-module-wrapper-manager-fds/"
+      interval = null
+    }
+    bahaghari = {
+      website  = "https://foo-dogsquared.github.io/bahaghari"
+      interval = null
+    }
+    portfolio = {
+      website  = "https://gallery.foodogsquared.one"
       interval = null
     }
   })
-  name = each.key
-  username = gitea_user.foodogsquared.username
-  mirror = true
-  migration_clone_address = "https://github.com/foo-dogsquared/${each.key}.git"
-  migration_service = "github"
+  name                         = each.key
+  username                     = gitea_user.foodogsquared.username
+  mirror                       = true
+  migration_clone_address      = "https://github.com/foo-dogsquared/${each.key}.git"
+  migration_service            = "github"
   migration_service_auth_token = var.github_clone_token
-  migration_mirror_interval = each.value.interval != null ? each.value.interval : "4h"
-  website = each.value.website
-  private = false
+  migration_mirror_interval    = each.value.interval != null ? each.value.interval : "4h"
+  website                      = each.value.website
+  private                      = false
 }
