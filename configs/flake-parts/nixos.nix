652aa9494b231907bea1d6ab2647b35336228e88
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-03-12 15:32:11 +0800

    hosts/ni: update foo-dogsquared description

--- configs/flake-parts/nixos.nix
+++ configs/flake-parts/nixos.nix
@@ -69,7 +69,7 @@ in {
               ];
               hashedPassword =
                 "$6$.cMYto0K0CHbpIMT$dRqyKs4q1ppzmTpdzy5FWP/V832a6X..FwM8CJ30ivK0nfLjQ7DubctxOZbeOtygfjcUd1PZ0nQoQpOg/WMvg.";
-              description = "Gabriel Arazas";
+              description = "Da-ba-dee-da-ba-di-da-ba-dee-da-ba-di";
             };
           };
         };
