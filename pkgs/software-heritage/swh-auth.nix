9c76f0318632d07764dfd5e98782e903bd9f2b24
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-03-25 09:43:37 +0800

    pkgs/swh/swh-auth: 0.7.2 -> 0.10.0

--- pkgs/software-heritage/swh-auth.nix
+++ pkgs/software-heritage/swh-auth.nix
@@ -4,33 +4,32 @@ with python3Packages;
 
 buildPythonPackage rec {
   pname = "swh-auth";
-  version = "0.7.2";
+  version = "0.10.0";
+  pyproject = true;
 
   src = fetchPypi {
     inherit version;
-    pname = "swh.auth";
-    sha256 = "sha256-f0++AuyJggoc19kPA/7UChbFjF/EoR+FztF00r5csLo=";
+    pname = "swh_auth";
+    hash = "sha256-J/5oFm0QSPNeEDKIYHEzSXWA/6uSOj9eu3LXYUTZjC0=";
   };
 
   # Tests require network access.
   doCheck = false;
 
   propagatedBuildInputs = [
-    django
-    djangorestframework
-    sentry-sdk
     click
     pyyaml
-    (python-keycloak.overrideAttrs (final: prev: rec {
-      version = "3.3.0";
-      src = pkgs.fetchPypi {
-        inherit version;
-        pname = "python_keycloak";
-        hash = "sha256-zIaBJvU1qk8yDcnqsk5GrzgcE7zIjZsHAbBCk+p1zSQ=";
-      };
-      propagatedBuildInputs = prev.propagatedBuildInputs
-        ++ [ setuptools deprecation ];
-    }))
+    python-keycloak
+
+    # Requirements for Django
+    django
+    djangorestframework
+    sentry-sdk_2
+
+    # Requirements for Starlette
+    starlette
+    httpx
+    aiocache
 
     swh-core
   ];
