fa3c08daafc8d196cc9ccb0304b60fbb800e9462
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-05-14 10:10:35 +0800

    users/foo-dogsquared/programs/nixvim: install manpage from main NixVim instance

--- configs/home-manager/foo-dogsquared/modules/programs/nixvim/default.nix
+++ configs/home-manager/foo-dogsquared/modules/programs/nixvim/default.nix
@@ -14,33 +14,41 @@ let
       module.imports = firstSetupArgs.baseNixvimModules ++ [ module ];
       extraSpecialArgs.hmConfig = config;
     };
+
+  # The main NixVim flavor and also where the extra files in the environment
+  # will come from.
+  nvimPkg = createNixvimFlavor {
+    imports = [
+      ./colorschemes.nix
+      ./fuzzy-finding.nix
+      ./misc.nix
+      ./note-taking.nix
+    ] ++ lib.optionals userCfg.setups.development.enable [
+      ./dev.nix
+      ./lsp.nix
+      ./dap.nix
+    ];
+
+    config = {
+      # Inherit all of the schemes.
+      bahaghari.tinted-theming.schemes =
+        hmCfg.bahaghari.tinted-theming.schemes;
+    };
+  };
+
+  nixvimManpage = pkgs.runCommand "get-main-nixvim-manpage" { } ''
+    mkdir -p $out/share && cp -r "${nvimPkg}/share/man" $out/share
+  '';
 in {
   options.users.foo-dogsquared.programs.nixvim.enable =
     lib.mkEnableOption "editors made with NixVim";
 
   config = lib.mkIf cfg.enable {
+    home.packages = [ nixvimManpage ];
+
     # Basically, we're creating Neovim flavors with NixVim so no need for it.
     wrapper-manager.packages.neovim-flavors = {
-      wrappers.nvim-fiesta.arg0 = let
-        nvimPkg = createNixvimFlavor {
-          imports = [
-            ./colorschemes.nix
-            ./fuzzy-finding.nix
-            ./misc.nix
-            ./note-taking.nix
-          ] ++ lib.optionals userCfg.setups.development.enable [
-            ./dev.nix
-            ./lsp.nix
-            ./dap.nix
-          ];
-
-          config = {
-            # Inherit all of the schemes.
-            bahaghari.tinted-theming.schemes =
-              hmCfg.bahaghari.tinted-theming.schemes;
-          };
-        };
-      in lib.getExe' nvimPkg "nvim";
+      wrappers.nvim-fiesta-fds.arg0 = lib.getExe' nvimPkg "nvim";
     };
   };
 }
