1ec01ea3dffda74b1af614edd9560f7fbd0e0e9f
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-05-21 17:36:53 +0800

    hosts/ni/setups/gaming: add Clone Hero and update Dwarf Fortress

--- configs/nixos/ni/modules/setups/gaming.nix
+++ configs/nixos/ni/modules/setups/gaming.nix
@@ -21,12 +21,32 @@ in {
       rpcs3
       ryubing
 
-      dwarf-fortress # Losing only means more possibilities to play.
+      clonehero # Free Minecraft note block player.
       mindustry # Not a Minecraft industry simulator.
       minetest # Free Minecraft.
       the-powder-toy # Free micro-Minecraft.
       rotp-foodogsquared # Free space Minecraft planet colonization simulator.
     ];
+
+    # Losing only means more possibilities to play.
+    programs.dwarf-fortress = {
+      enable = true;
+      wrapperSettings = {
+        enableIntro = true;
+        enableFPS = true;
+      };
+    };
+
+    xdg.desktopEntries.dwarf-fortress = {
+      desktopName = "Dwarf Fortress";
+      exec = lib.getExe config.programs.dwarf-fortress.package;
+      genericName = "Dwarf Fortress";
+      terminal = true;
+      icon = pkgs.fetchurl {
+        url = "https://upload.wikimedia.org/wikipedia/commons/a/a9/Dwarf_Fortress_Icon.svg";
+        hash = "sha256-bnNCf7CuiwlnsEdcBEwbqm5d/1z2xgxmGRdBC9sevmE=";
+      };
+    };
 
     # This is somewhat used for streaming games from it.
     programs.steam.remotePlay.openFirewall = true;
