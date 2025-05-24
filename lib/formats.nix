997939c9621d52482055af4e4ae1cb6bbac10007
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-05-24 19:38:51 +0800

    lib/formats: add dconfINI
    Though, it is basically just the same as GLib keyfile.

--- lib/formats.nix
+++ lib/formats.nix
@@ -1,10 +1,12 @@
 { lib, pkgs, self }:
 
-{
+rec {
   # The gnome-session config files uses one from GLib. See the following link
   # at <https://docs.gtk.org/glib/struct.KeyFile.html> for details about the
   # keyfile formatting and possibly the Desktop Entry specification at
   # <https://freedesktop.org/wiki/Specifications/desktop-entry-spec>.
+  #
+  # Anyways, this is just one of the many possible ways to configure GSettings.
   glibKeyfile = {}: {
     type = with lib.types;
       let
@@ -18,4 +20,8 @@
       pkgs.callPackage
       ({ lib, writeText }: writeText name (lib.generators.toDconfINI value));
   };
+
+  # A dconf keyfile as far as I can understand it is a GLib keyfile-like format
+  # so it is acceptable to have it the same as GLib keyfile format.
+  dconfINI = glibKeyfile;
 }
