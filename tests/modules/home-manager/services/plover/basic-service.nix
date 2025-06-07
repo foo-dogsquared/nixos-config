72f9dea2bfaca5bc731359cfe5a09b51b3a11bf5
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-06-07 21:25:10 +0800

    tests/home-manager: update test stubs

--- tests/modules/home-manager/services/plover/basic-service.nix
+++ tests/modules/home-manager/services/plover/basic-service.nix
@@ -3,15 +3,12 @@
 {
   services.plover = {
     enable = true;
-    package = pkgs.plover-dev;
     settings = {
       "Output Configuration" = { undo_levels = 100; };
 
       "Stroke Display" = { show = true; };
     };
   };
-
-  test.stubs.plover-dev = { };
 
   nmt.script = ''
     assertFileExists home-files/.config/plover/plover.cfg
