292e5a4aa03a46835e3bcb1b7ca10528b2872b63
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-06-07 18:51:32 +0800

    users/foo-dogsquared/programs/terminal-emulator: add dropdown option

--- configs/home-manager/foo-dogsquared/modules/programs/terminal-emulator.nix
+++ configs/home-manager/foo-dogsquared/modules/programs/terminal-emulator.nix
@@ -25,18 +25,6 @@ let
     type = "Application";
     categories = [ "System" "TerminalEmulator" "Utility" ];
   };
-  weztermTypicalDesktop = pkgs.makeDesktopItem {
-    name = "wezterm-start";
-    desktopName = "WezTerm";
-    comment = "Wez's Terminal Emulator";
-    keywords = [ "shell" "prompt" "command" "commandline" "cmd" ];
-    icon = "org.wezfurlong.wezterm";
-    startupWMClass = "org.wezfurlong.wezterm";
-    tryExec = "wezterm";
-    exec = "wezterm start --cwd .";
-    type = "Application";
-    categories = [ "System" "TerminalEmulator" "Utility" ];
-  };
 in {
   options.users.foo-dogsquared.programs.terminal-emulator.enable =
     lib.mkEnableOption "foo-dogsquared's terminal emulator setup";
@@ -47,10 +35,58 @@ in {
       # the user has them.
       home.packages = let
         inherit (pkgs) wezterm hiPrio;
-      in [ wezterm (hiPrio weztermUserDefaultDesktop) weztermTypicalDesktop ];
+      in [ wezterm (hiPrio weztermUserDefaultDesktop) ];
 
       xdg.autostart.entries =
         lib.singleton (getXdgDesktop weztermUserDefaultDesktop "org.wezfurlong.wezterm");
+
+      # Based from https://mwop.net/blog/2024-09-17-wezterm-dropdown.html
+      wrapper-manager.packages.wezterm-wrappers = { config, ... }: let
+        dropdownName = "one.foodogsquared.WeztermDropDown";
+      in {
+        wrappers.${dropdownName} = {
+          arg0 = lib.getExe' pkgs.wezterm "wezterm";
+          prependArgs = let
+            configs = [
+              "window_decorations='NONE'"
+              "window_background_opacity=0.875"
+            ];
+            mkConfigs = c: [
+              "--config" c
+            ];
+          in
+            lib.concatMap mkConfigs configs
+            ++ [
+              "start"
+              "--cwd" "."
+              "--class" dropdownName
+              "--domain" "unix"
+              "--attach"
+              "--workspace dropdown"
+            ];
+        };
+
+        xdg.desktopEntries."${dropdownName}" = {
+          desktopName = "Wezterm (dropdown)";
+          exec = config.wrappers.${dropdownName}.executableName;
+          tryExec = "wezterm";
+          icon = "org.wezfurlong.wezterm";
+          categories = [ "System" "TerminalEmulator" "Utility" ];
+          comment = "Open Wezterm as a dropdown terminal";
+          startupWMClass = dropdownName;
+        };
+
+        xdg.desktopEntries."wezterm-start" = {
+          desktopName = "WezTerm";
+          tryExec = "wezterm";
+          exec = "wezterm start --cwd .";
+          comment = "Wez's Terminal Emulator";
+          keywords = [ "shell" "prompt" "command" "commandline" "cmd" ];
+          icon = "org.wezfurlong.wezterm";
+          startupWMClass = "org.wezfurlong.wezterm";
+          categories = [ "System" "TerminalEmulator" "Utility" ];
+        };
+      };
     }
 
     (lib.mkIf (!hasNixosModuleEnable) {
